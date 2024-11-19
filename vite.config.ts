61adf5eb011fb7057dc82a66b9e1847721c7c21d
Commeci <cjtkemfldj@naver.com>
2024-11-19 17:43:31 +0900

    📦 Build: svgr 라이브러리 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from "tailwindcss";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   css: {
     postcss: {
-      plugins: [tailwindcss()]
-    }
-  }
-})
+      plugins: [tailwindcss()],
+    },
+  },
+});
