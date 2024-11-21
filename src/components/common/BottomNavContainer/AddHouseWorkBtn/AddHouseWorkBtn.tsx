2c35128007321a1f946ee8a1855dbd706c365d9b
Commeci <cjtkemfldj@naver.com>
2024-11-21 12:25:19 +0900

    ✨ Feat: 집안일 추가 버튼 컴포넌트 추가

--- src/components/common/BottomNavContainer/AddHouseWorkBtn/AddHouseWorkBtn.tsx
+++ src/components/common/BottomNavContainer/AddHouseWorkBtn/AddHouseWorkBtn.tsx
@@ -1,5 +1,12 @@
 const AddHouseWorkBtn = () => {
-  return <div>AddHouseWorkBtn</div>;
+  return (
+    <button
+      className='bg-custom-gradient text-white01 h-12 w-12 rounded-full'
+      onClick={() => console.log('나중에 루트')}
+    >
+      +
+    </button>
+  );
 };
 
 export default AddHouseWorkBtn;
