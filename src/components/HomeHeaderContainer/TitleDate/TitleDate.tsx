dde89a30f2aa8f182b965553ff8d2585f729a25f
yook <si89336@naver.com>
2024-11-21 12:19:53 +0900

    💄 Style: TitleDate 퍼블리싱

--- src/components/HomeHeaderContainer/TitleDate/TitleDate.tsx
+++ src/components/HomeHeaderContainer/TitleDate/TitleDate.tsx
@@ -1,5 +1,12 @@
-const TitleDate = () => {
-  return <div>TitleDate</div>;
+import React from 'react';
+
+interface TitleDateProps {
+  /** 타이틀 날짜의 내용 */
+  dateText: string;
+}
+
+const TitleDate: React.FC<TitleDateProps> = ({ dateText }: TitleDateProps) => {
+  return <div className='text-gray02'>{dateText}</div>;
 };
 
 export default TitleDate;
