39be4c49eb4734c6354b38c046c50966ad8898dd
yook <si89336@naver.com>
2024-11-21 12:32:16 +0900

    💄 Style: HomeHeaderContainer 퍼블리싱

--- src/components/HomeHeaderContainer/HomeHeaderContainer.tsx
+++ src/components/HomeHeaderContainer/HomeHeaderContainer.tsx
@@ -1,5 +1,13 @@
+import GroupSelectBtn from './GroupSelectBtn/GroupSelectBtn';
+import TitleDate from './TitleDate/TitleDate';
+
 const HomeHeaderContainer = () => {
-  return <div>HomeHeaderContainer</div>;
+  return (
+    <div className='bg-white02 flex items-center justify-between p-5'>
+      <TitleDate dateText='2024년 11월 둘째 주' />
+      <GroupSelectBtn groupName='우리집' />
+    </div>
+  );
 };
 
 export default HomeHeaderContainer;
