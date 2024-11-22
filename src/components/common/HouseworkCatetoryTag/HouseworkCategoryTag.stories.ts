b9aaa00a19325dfc7ce609531c726cd77d38dec0
Eugeeun <jack603@naver.com>
2024-11-22 11:32:49 +0900

    📝 Docs: 집안일 카테고리 태그 스토리북 문서 추가

---
+++ src/components/common/HouseworkCatetoryTag/HouseworkCategoryTag.stories.ts
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import HouseworkCategoryTag, {
+  HouseworkCategoryTagProps,
+} from '@/components/common/HouseworkCatetoryTag/HouseworkCategoryTag';
+
+const meta = {
+  title: 'components/HouseworkCategoryTag',
+  component: HouseworkCategoryTag,
+  tags: ['autodocs'],
+} satisfies Meta<typeof HouseworkCategoryTag>;
+
+export default meta;
+
+type Story = StoryObj<HouseworkCategoryTagProps>;
+
+export const Default: Story = {
+  args: {
+    category: '거실',
+  },
+};
