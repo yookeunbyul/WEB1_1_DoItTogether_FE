a584872154b650108d5fb4c77e7cddbce78265f1
yook <si89336@naver.com>
2024-11-22 11:48:51 +0900

    💄 Style: GroupOptionContainer 퍼블리싱

---
+++ src/components/GroupOptionContainer/GroupOptionContainer.tsx
@@ -0,0 +1,14 @@
+import GroupOption from '@/components/GroupOptionContainer/GroupOption/GroupOption';
+
+const GroupOptionContainer = () => {
+  const groups = ['우리집', '회사'];
+  return (
+    <ul className='flex flex-col gap-y-6 px-5 pb-14 pt-8'>
+      {groups.map(group => (
+        <GroupOption key={group} groupName={group} />
+      ))}
+    </ul>
+  );
+};
+
+export default GroupOptionContainer;
