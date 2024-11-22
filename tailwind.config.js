dd654e14fb62632dfadd50c6b477c471c92b740e
yook <si89336@naver.com>
2024-11-22 09:46:30 +0900

    📦 shadcn-ui 설치 및 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,85 @@
 /** @type {import('tailwindcss').Config} */
 
 export default {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+    darkMode: ['class'],
+    content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {
-      fontFamily: {
-        Pretendard: ['Pretendard'],
-      },
-      backgroundImage: {
-        'custom-gradient': 'linear-gradient(to right, rgba(72, 77, 84, 0.5), rgba(72, 77, 84, 1))',
-      },
-    },
-    colors: {
-      black01: '#212121',
-      black02: '#464656',
-      gray01: '#626273',
-      gray02: '#9090A0',
-      gray03: '#C6C6D0',
-      white01: '#E9E9EE',
-      white02: '#F5F5F9',
-      white03: '#FFFFFF',
-    },
-    fontSize: {
-      12: '0.75rem',
-      14: '0.875rem',
-      16: '1rem',
-      18: '1.25rem',
-      24: '1.5rem',
-      28: '1.75rem',
-    },
-    maxWidth: {
-      DEFAULT: '430px',
-    },
+  	extend: {
+  		fontFamily: {
+  			Pretendard: ['Pretendard']
+  		},
+  		backgroundImage: {
+  			'custom-gradient': 'linear-gradient(to right, rgba(72, 77, 84, 0.5), rgba(72, 77, 84, 1))'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		}
+  	},
+  	colors: {
+  		black01: '#212121',
+  		black02: '#464656',
+  		gray01: '#626273',
+  		gray02: '#9090A0',
+  		gray03: '#C6C6D0',
+  		white01: '#E9E9EE',
+  		white02: '#F5F5F9',
+  		white03: '#FFFFFF'
+  	},
+  	fontSize: {
+  		'12': '0.75rem',
+  		'14': '0.875rem',
+  		'16': '1rem',
+  		'18': '1.25rem',
+  		'24': '1.5rem',
+  		'28': '1.75rem'
+  	},
+  	maxWidth: {
+  		DEFAULT: '430px'
+  	}
   },
   plugins: [
     function ({ addUtilities }) {
@@ -46,5 +94,6 @@ export default {
       };
       addUtilities(newUtilities);
     },
-  ],
+      require("tailwindcss-animate")
+],
 };
