c40ce550b14670b03ca06484d83205d1c5338732
yook <si89336@naver.com>
2024-11-25 15:25:35 +0900

    💄 Style: MainLayout 설정

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,5 +1,17 @@
+import BottomNavContainer from '@/components/common/BottomNavContainer/BottomNavContainer';
+import { Outlet } from 'react-router-dom';
+
 const MainLayout = () => {
-  return <div>MainLayout</div>;
+  return (
+    <div className='flex min-h-screen flex-col'>
+      <div className='flex-1'>
+        <Outlet />
+      </div>
+      <div className='bg-white fixed bottom-0 w-full'>
+        <BottomNavContainer />
+      </div>
+    </div>
+  );
 };
 
 export default MainLayout;
