1abfc36ce76772729011e9f38f1a49fe810f6845
yook <si89336@naver.com>
2024-11-25 16:36:51 +0900

    💄 Style: 바텀시트 > 그룹 변경 컴포넌트 추가

--- src/components/HomeHeaderContainer/GroupSelectBtn/GroupSelectBtn.tsx
+++ src/components/HomeHeaderContainer/GroupSelectBtn/GroupSelectBtn.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import BottomSheetContainer from '@/components/common/BottomSheetContainer/BottomSheetContainer';
+import GroupOptionContainer from '@/components/GroupOptionContainer/GroupOptionContainer';
+import React, { useState } from 'react';
 
 interface GroupSelectBtnProps {
   /** 현재 속해있는 그룹명 */
@@ -6,11 +8,25 @@ interface GroupSelectBtnProps {
 }
 
 const GroupSelectBtn: React.FC<GroupSelectBtnProps> = ({ groupName }: GroupSelectBtnProps) => {
+  const [isOpen, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
   return (
-    <button className='bg-gray03 text-12 text-white03 flex items-center rounded-full px-2 py-1'>
-      <div className='bg-gray01 h-4 w-4 rounded-full'></div>
-      <div className='pl-2'>{groupName}</div>
-    </button>
+    <>
+      <button
+        className='text-12 flex items-center rounded-full bg-gray03 px-2 py-1 text-white03'
+        onClick={handleClick}
+      >
+        <div className='h-4 w-4 rounded-full bg-gray01'></div>
+        <div className='pl-2'>{groupName}</div>
+      </button>
+
+      <BottomSheetContainer isOpen={isOpen} setOpen={setOpen} title='그룹 변경'>
+        <GroupOptionContainer />
+      </BottomSheetContainer>
+    </>
   );
 };
 
