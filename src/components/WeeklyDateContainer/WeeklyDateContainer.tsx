99ce8614ba9b138864f464e11c262bfc5254edcb
Eugeeun <jack603@naver.com>
2024-11-26 00:31:17 +0900

    ✨ Feat: 현재 날짜 가져와서 자동 active

--- src/components/WeeklyDateContainer/WeeklyDateContainer.tsx
+++ src/components/WeeklyDateContainer/WeeklyDateContainer.tsx
@@ -1,21 +1,35 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import DateItem from './DateItem/DateItem';
 
 const WeeklyDateContainer = () => {
   const dummyItems = [
-    { date: 18, day: '월', pendingCnt: 2 },
-    { date: 19, day: '화', pendingCnt: 0 },
-    { date: 20, day: '수', pendingCnt: 1 },
-    { date: 21, day: '목', pendingCnt: 4 },
-    { date: 22, day: '금', pendingCnt: 3 },
-    { date: 23, day: '토', pendingCnt: 0 },
-    { date: 24, day: '일', pendingCnt: 4 },
+    { date: 25, day: '월', pendingCnt: 2 },
+    { date: 26, day: '화', pendingCnt: 0 },
+    { date: 27, day: '수', pendingCnt: 1 },
+    { date: 28, day: '목', pendingCnt: 4 },
+    { date: 29, day: '금', pendingCnt: 3 },
+    { date: 30, day: '토', pendingCnt: 0 },
+    { date: 1, day: '일', pendingCnt: 4 },
   ];
 
   const [activeDate, setActiveDate] = useState<number>();
+
+  useEffect(() => {
+    const today = new Date();
+    const currentDate = today.getDate();
+
+    const todayIndex = dummyItems.findIndex(item => item.date === currentDate);
+
+    if (todayIndex !== -1) {
+      setActiveDate(todayIndex);
+    }
+  }, []);
+
   const handleActiveDate = (idx: number) => {
     setActiveDate(idx);
   };
+
+  console.log(activeDate);
 
   return (
     <div className='flex items-center justify-between bg-white03 px-5 py-2'>
