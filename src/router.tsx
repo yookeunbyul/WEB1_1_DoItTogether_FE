c51d7118b971e2ddd7f67435d20ab1eeb2d83b2d
yook <si89336@naver.com>
2024-11-25 12:44:50 +0900

    🔧 Wrench: 라우팅 추가 설정

--- src/router.tsx
+++ src/router.tsx
@@ -64,7 +64,7 @@ export const router = createBrowserRouter([
     element: <MainLayout />,
     children: [
       {
-        path: 'home',
+        index: true,
         element: <HomePage />,
       },
       {
