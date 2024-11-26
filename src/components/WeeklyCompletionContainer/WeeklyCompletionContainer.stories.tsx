fc345eadecbd6865c659a0badbff2174edcb026c
Eugeeun <jack603@naver.com>
2024-11-26 11:45:42 +0900

    📝 Docs: 주간 완료 컨테이너 스토리북 문서 추가

---
+++ src/components/WeeklyCompletionContainer/WeeklyCompletionContainer.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import WeeklyCompletionContainer from '@/components/WeeklyCompletionContainer/WeeklyCompletionContainer';
+
+const meta = {
+  title: 'components/WeeklyCompletionContainer/WeeklyCompletionContainer',
+  component: WeeklyCompletionContainer,
+  tags: ['autodocs'],
+} satisfies Meta<typeof WeeklyCompletionContainer>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    groupName: '우리집',
+    completed: 2,
+    notCompleted: 2,
+  },
+};
