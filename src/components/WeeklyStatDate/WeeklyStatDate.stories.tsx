e99e832e0b830d3fec0124d8c8b769e0a1600e9f
Eugeeun <jack603@naver.com>
2024-11-26 10:59:43 +0900

    📝 Docs: 주간 통계 날짜 컴포넌트 스토리북 문서 수정

--- src/components/WeeklyStatDate/WeeklyStatDate.stories.tsx
+++ src/components/WeeklyStatDate/WeeklyStatDate.stories.tsx
@@ -1,6 +1,6 @@
+import { useState } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
-import WeeklyStatDate, { WeeklyStatDateProps } from '@/components/WeeklyStatDate/WeeklyStatDate';
-import { action } from '@storybook/addon-actions';
+import WeeklyStatDate from './WeeklyStatDate'; // 경로에 맞게 수정
 
 const meta = {
   title: 'components/WeeklyStatDate',
@@ -10,12 +10,35 @@ const meta = {
 
 export default meta;
 
-type Story = StoryObj<WeeklyStatDateProps>;
+type Story = StoryObj<typeof WeeklyStatDate>;
 
 export const Default: Story = {
-  args: {
-    currentDate: new Date(),
-    handlePrevWeek: action('prev-btn-clicked'),
-    handleNextWeek: action('next-btn-clicked'),
+  render: () => {
+    // 상태 관리
+    const [currentDate, setCurrentDate] = useState(new Date());
+
+    const handlePrevWeek = () => {
+      setCurrentDate(prevDate => {
+        const newDate = new Date(prevDate);
+        newDate.setDate(newDate.getDate() - 7);
+        return newDate;
+      });
+    };
+
+    const handleNextWeek = () => {
+      setCurrentDate(prevDate => {
+        const newDate = new Date(prevDate);
+        newDate.setDate(newDate.getDate() + 7);
+        return newDate;
+      });
+    };
+
+    return (
+      <WeeklyStatDate
+        currentDate={currentDate}
+        handlePrevWeek={handlePrevWeek}
+        handleNextWeek={handleNextWeek}
+      />
+    );
   },
 };
