78e0e3be3ae2f6a5877985e93ad4829e640b3d16
Commeci <cjtkemfldj@naver.com>
2024-11-26 09:51:35 +0900

    ✨ Feat: props로 tab목록 받도록 처리

--- src/components/common/TabContainer/TabContainer.tsx
+++ src/components/common/TabContainer/TabContainer.tsx
@@ -2,26 +2,22 @@ import React from 'react';
 import Tab from '@/components/common/TabContainer/Tab/Tab';
 import { Tabs, TabsList } from '@/components/ui/tabs';
 
-const DUMMY_MEMBERS = [
-  { name: '전체', value: 'all' },
-  { name: '홍길동', value: 'hong' },
-  { name: '김철수', value: 'kim' },
-  { name: '이영희', value: 'lee' },
-  { name: '박지성', value: 'park' },
-  { name: '최민수', value: 'choi' },
-];
+interface Charger {
+  name: string;
+}
 
 interface TabContainerProps {
   activeTab: string;
   handleSetActiveTab: React.Dispatch<React.SetStateAction<string>>;
+  chargers: Charger[];
 }
 
-const TabContainer: React.FC<TabContainerProps> = ({ activeTab, handleSetActiveTab }) => {
+const TabContainer: React.FC<TabContainerProps> = ({ activeTab, handleSetActiveTab, chargers }) => {
   return (
     <Tabs defaultValue={activeTab} onValueChange={handleSetActiveTab}>
       <TabsList className='flex h-9 w-full justify-start overflow-x-auto overflow-y-hidden rounded-none bg-white03 p-0 px-5 no-scrollbar'>
-        {DUMMY_MEMBERS.map(member => (
-          <Tab key={member.value} name={member.name} value={member.name} />
+        {chargers?.map(charger => (
+          <Tab key={charger.name} name={charger.name} value={charger.name} />
         ))}
       </TabsList>
     </Tabs>
