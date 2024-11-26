b9b6cf11c417a41a524b42787bda97cdfb595904
Commeci <cjtkemfldj@naver.com>
2024-11-26 15:07:57 +0900

    ✨ Feat: 커스텀 할 수 있도록 기능 추가

--- src/components/common/ButtonContainer/Button/Button.tsx
+++ src/components/common/ButtonContainer/Button/Button.tsx
@@ -9,11 +9,13 @@ interface ButtonProps {
   size: 'small' | 'medium' | 'large';
   /** 클릭 이벤트 */
   handleClick?: () => void;
+  /** 커스텀 스타일 */
+  className?: string;
 }
 
-const Button = ({ label, variant = 'full', size, handleClick }: ButtonProps) => {
+const Button = ({ label, variant = 'full', size, handleClick, className }: ButtonProps) => {
   return (
-    <ButtonComponent variant={variant} size={size} onClick={handleClick}>
+    <ButtonComponent variant={variant} size={size} onClick={handleClick} className={className}>
       {label}
     </ButtonComponent>
   );
