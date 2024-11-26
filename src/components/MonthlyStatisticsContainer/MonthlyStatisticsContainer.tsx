503d7e7d03ec2a954f7ad3ad9d61699adce80714
Commeci <cjtkemfldj@naver.com>
2024-11-26 16:14:21 +0900

    ✨ Feat: 월간 통계 컨테이너 파일 추가

---
+++ src/components/MonthlyStatisticsContainer/MonthlyStatisticsContainer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface MonthlyStatisticsContainerProps {}
+
+const MonthlyStatisticsContainer: React.FC<MonthlyStatisticsContainerProps> = ({}) => {
+  return (
+    <div>
+      <p>월간 통계다~~~~</p>
+    </div>
+  );
+};
+
+export default MonthlyStatisticsContainer;
