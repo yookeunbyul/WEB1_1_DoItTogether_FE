6abc71e502ded765921d7c977c8532130611eae5
ayoung <a01259@naver.com>
2024-11-27 16:44:56 +0900

    ✨ Feat: 온보딩 인트로 아이콘 컴포넌트 추가

---
+++ src/components/survey/surveyIntro/SurveyIntroIcon.tsx
@@ -0,0 +1,12 @@
+const SurveyIntroIcon = () => {
+  {
+    /* TODO 그래픽요소로 변경*/
+  }
+  return (
+    <div className='flex items-center justify-center'>
+      <div className='h-56 w-56 rounded-full border bg-gray03' />
+    </div>
+  );
+};
+
+export default SurveyIntroIcon;
