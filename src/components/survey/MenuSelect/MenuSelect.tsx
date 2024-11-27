9482b0404dea1c41e2a7bfd702cbd139a495c8da
Eugeeun <jack603@naver.com>
2024-11-27 17:29:48 +0900

    ✨ Feat: 스텝1 메뉴 선택 추가

--- src/components/survey/MenuSelect/MenuSelect.tsx
+++ src/components/survey/MenuSelect/MenuSelect.tsx
@@ -5,14 +5,16 @@ interface MenuSelectProps {
   type: 'full' | 'tight';
   status: 'active' | 'inActive';
   content: string;
+  handleSelect: (content: string) => void;
 }
 
-const MenuSelect: React.FC<MenuSelectProps> = ({ type, status, content }) => {
+const MenuSelect: React.FC<MenuSelectProps> = ({ type, status, content, handleSelect }) => {
   return (
     <Button
       className={`max-w rounded-full ${type === 'full' && 'justify-start'} ${status === 'active' && 'bg-black01 text-white03'} px-5 py-3`}
       size={`${type === 'full' ? 'full' : 'tight'}`}
       variant={'select'}
+      onClick={() => handleSelect(content)}
     >
       {content}
     </Button>
