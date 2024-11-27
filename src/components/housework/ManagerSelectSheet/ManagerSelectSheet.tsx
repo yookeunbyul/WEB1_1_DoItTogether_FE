42f3e2e221f7c0495afe59dbbfeb605b60cb7bd8
Eugeeun <jack603@naver.com>
2024-11-27 23:36:57 +0900

    ✨ Feat: 세로 버튼에서 가로 버튼으로 수정

--- src/components/housework/ManagerSelectSheet/ManagerSelectSheet.tsx
+++ src/components/housework/ManagerSelectSheet/ManagerSelectSheet.tsx
@@ -3,7 +3,6 @@ import { Dispatch, SetStateAction } from 'react';
 import BottomSheet from '@/components/common/bottomSheet/BottomSheet';
 import ManagerItems from '@/components/housework/ManagerSelectSheet/ManagerItem/ManagerItems';
 import Button from '@/components/common/button/Button/Button';
-import UnderlineBtn from '@/components/common/button/UnderlineBtn/UnderlineBtn';
 
 interface ManagerSelectSheetProps {
   /**바텀시트 오픈 여부 */
@@ -30,8 +29,10 @@ const ManagerSelectSheet: React.FC<ManagerSelectSheetProps> = ({
     <BottomSheet isOpen={isOpen} setOpen={setIsOpen} title='담당자 고르기'>
       <div className='flex flex-col gap-y-6 px-5 pb-6'>
         <ManagerItems selectedMember={selectedMember} handleSelectMember={handleSetSelectMember} />
-        <Button label='완료' variant='full' size='large' handleClick={handleDoneClick} />
-        <UnderlineBtn text='AI가 딱 맞는 사람을 선택할게요' handleClick={handleClick} />
+        <div className='flex gap-3'>
+          <Button label='ai 선택' variant='outline' size='large' handleClick={handleClick} />
+          <Button label='완료' variant='full' size='large' handleClick={handleDoneClick} />
+        </div>
       </div>
     </BottomSheet>
   );
