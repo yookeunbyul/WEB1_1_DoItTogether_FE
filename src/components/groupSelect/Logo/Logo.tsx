b455b4ea190d47dc6296f41db1f583a8c985582f
yook <si89336@naver.com>
2024-11-27 14:41:49 +0900

    💄 Style: padding 추가

--- src/components/groupSelect/Logo/Logo.tsx
+++ src/components/groupSelect/Logo/Logo.tsx
@@ -1,5 +1,9 @@
 const Logo = () => {
-  return <div className='h-8 w-8 bg-gray03'></div>;
+  return (
+    <div className='px-5'>
+      <div className='h-8 w-8 bg-gray03'></div>
+    </div>
+  );
 };
 
 export default Logo;
