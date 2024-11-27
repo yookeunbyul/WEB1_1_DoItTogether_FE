0f8c701f3f5c0248f5c00f8de7e5ffb3b6f563fb
Commeci <cjtkemfldj@naver.com>
2024-11-27 09:43:18 +0900

    🔧 Wrench: input 스토리북 수정

--- src/components/common/input/InputBox.stories.tsx
+++ src/components/common/input/InputBox.stories.tsx
@@ -1,26 +1,31 @@
+// InputBox.stories.tsx
+import InputBox from '@/components/common/input/InputBox';
 import type { Meta, StoryObj } from '@storybook/react';
-import InputBox from './InputBox';
 
-const meta: Meta<typeof InputBox> = {
-  title: 'components/common/input',
+const meta = {
+  title: 'components/common/InputBox',
   component: InputBox,
   tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
   argTypes: {
     value: { control: 'text' },
     placeholder: { control: 'text' },
     disabled: { control: 'boolean' },
+    handleChange: { action: 'changed' },
   },
-};
+} satisfies Meta<typeof InputBox>;
 
 export default meta;
-
-type Story = StoryObj<typeof InputBox>;
+type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
     value: '기본 텍스트',
     placeholder: '뭐든 입력',
     disabled: false,
+    handleChange: undefined,
   },
 };
 
@@ -29,6 +34,7 @@ export const EmptyText: Story = {
     value: '',
     placeholder: '뭐든 입력',
     disabled: false,
+    handleChange: undefined,
   },
 };
 
@@ -37,5 +43,15 @@ export const Disabled: Story = {
     value: '비활성화된 텍스트',
     placeholder: '입력 불가',
     disabled: true,
+    handleChange: undefined,
+  },
+};
+
+export const WithHandler: Story = {
+  args: {
+    value: '',
+    placeholder: '입력해보세요',
+    disabled: false,
+    handleChange: value => console.log('Input changed:', value),
   },
 };
