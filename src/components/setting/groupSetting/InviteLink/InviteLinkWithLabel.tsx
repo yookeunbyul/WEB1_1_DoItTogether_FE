c73c41536246a4c32813678087183d6da93bdb3a
Commeci <cjtkemfldj@naver.com>
2024-11-27 23:53:13 +0900

    ✨ Feat: 초대 링크 컴포넌트 props 수정

--- src/components/setting/groupSetting/InviteLink/InviteLinkWithLabel.tsx
+++ src/components/setting/groupSetting/InviteLink/InviteLinkWithLabel.tsx
@@ -1,10 +1,15 @@
 import InviteLink from '@/components/setting/groupSetting/InviteLink/InviteLink';
+import React from 'react';
 
-const InviteLinkWithLabel = () => {
+interface InviteLinkWithLabelProps {
+  initialLink?: string;
+}
+
+const InviteLinkWithLabel: React.FC<InviteLinkWithLabelProps> = ({ initialLink }) => {
   return (
     <div className='flex flex-col gap-2'>
       <p className='text-14'>초대 링크</p>
-      <InviteLink />
+      <InviteLink initialLink={initialLink} />
     </div>
   );
 };
