a3b18de20e2ab49a5df4bef9ba71b6e0ff29c35d
ayoung <a01259@naver.com>
2024-11-27 11:26:56 +0900

    ✨ Feat: 회원가입 타이틀 컴포넌트 추가

---
+++ src/components/register/RegisterTitle.tsx
@@ -0,0 +1,11 @@
+const RegisterTitle = () => {
+  return (
+    <p className='text-20 text-center font-semibold text-black02'>
+      사용하실 닉네임과
+      <br />
+      프로필을 설정해주세요
+    </p>
+  );
+};
+
+export default RegisterTitle;
