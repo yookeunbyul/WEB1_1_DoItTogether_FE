2317635d84eb69c2f51812c1a2eb8aa5cebd2763
yook <si89336@naver.com>
2024-11-27 14:58:27 +0900

    💄 Style: whitespace-pre-line 추가

--- src/components/groupSelect/GroupSelectTitle/GroupSelectTitle.tsx
+++ src/components/groupSelect/GroupSelectTitle/GroupSelectTitle.tsx
@@ -1,8 +1,7 @@
 const GroupSelectTitle = () => {
   return (
-    <div className='text-20 px-5'>
-      <p>번거로운 집안일,</p>
-      <p>두잇투게더와 함께 시작해보세요!</p>
+    <div className='text-20 whitespace-pre-line px-5'>
+      {'번거로운 집안일,\n두잇투게더와 함께 시작해보세요!'}
     </div>
   );
 };
