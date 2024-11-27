9a97851d830370600e49ef8442e8f4a4abdb2b89
yook <si89336@naver.com>
2024-11-27 22:01:02 +0900

    💄 Style: UnderlineBtn 적용

--- src/components/housework/ManagerSelectSheet/ManagerSelectSheet.tsx
+++ src/components/housework/ManagerSelectSheet/ManagerSelectSheet.tsx
@@ -3,6 +3,7 @@ import { Dispatch, SetStateAction } from 'react';
 import BottomSheet from '@/components/common/bottomSheet/BottomSheet';
 import ManagerItems from '@/components/housework/ManagerSelectSheet/ManagerItem/ManagerItems';
 import Button from '@/components/common/button/Button/Button';
+import UnderlineBtn from '@/components/common/button/UnderlineBtn/UnderlineBtn';
 
 interface ManagerSelectSheetProps {
   /**바텀시트 오픈 여부 */
@@ -24,12 +25,13 @@ const ManagerSelectSheet: React.FC<ManagerSelectSheetProps> = ({
   handleSetSelectMember,
   handleDoneClick,
 }) => {
+  const handleClick = () => {};
   return (
     <BottomSheet isOpen={isOpen} setOpen={setIsOpen} title='담당자 고르기'>
       <div className='flex flex-col gap-y-6 px-5 pb-6'>
         <ManagerItems selectedMember={selectedMember} handleSelectMember={handleSetSelectMember} />
         <Button label='완료' variant='full' size='large' handleClick={handleDoneClick} />
-        <button className='text-14 underline'>AI가 딱 맞는 사람을 선택할게요</button>
+        <UnderlineBtn text='AI가 딱 맞는 사람을 선택할게요' handleClick={handleClick} />
       </div>
     </BottomSheet>
   );
