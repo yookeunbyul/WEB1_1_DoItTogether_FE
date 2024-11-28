aa51dffe5a15b451ab5d056708b726a7e2f174e6
yook <si89336@naver.com>
2024-11-28 10:00:01 +0900

    💄 Style: AccountSetBtn 퍼블리싱

---
+++ src/components/my/AccountSetBtn/AccountSetBtn.tsx
@@ -0,0 +1,10 @@
+const AccountSetBtn = () => {
+  const handleClick = () => {};
+  return (
+    <div className='flex justify-end px-5 py-4'>
+      <button className='h-5 w-5 bg-gray01' onClick={handleClick}></button>
+    </div>
+  );
+};
+
+export default AccountSetBtn;
