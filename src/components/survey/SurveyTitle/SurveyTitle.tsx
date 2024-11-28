118a1014df9bbfab8eeeb234b22fb669d855d7fd
ayoung <a01259@naver.com>
2024-11-28 11:36:24 +0900

    ✨ Feat: 온보딩 타이틀컴포넌트 props 수정

--- src/components/survey/SurveyTitle/SurveyTitle.tsx
+++ src/components/survey/SurveyTitle/SurveyTitle.tsx
@@ -1,9 +1,9 @@
 interface SurveyTitleProps {
-  title: string;
+  title: string | JSX.Element;
 }
 
 const SurveyTitle = ({ title }: SurveyTitleProps) => {
-  return <p className='whitespace-pre-line text-24 font-semibold text-black02'>{title}</p>;
+  return <p className='whitespace-pre-line text-20 font-semibold text-black02'>{title}</p>;
 };
 
 export default SurveyTitle;
