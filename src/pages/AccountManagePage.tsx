f7e852dd345a306a987b47346c7d5d1e96ffc1f7
Commeci <cjtkemfldj@naver.com>
2024-11-28 11:50:10 +0900

    ✨ Feat: isNeededBackBtn props 삭제

--- src/pages/AccountManagePage.tsx
+++ src/pages/AccountManagePage.tsx
@@ -18,12 +18,7 @@ const AccountManagePage = () => {
 
   return (
     <div>
-      <Header
-        title='계정 관리'
-        isNeededBackBtn={true}
-        isNeededDoneBtn={false}
-        handleBack={handleBack}
-      />
+      <Header title='계정 관리' isNeededDoneBtn={false} handleBack={handleBack} />
       <AccountMenuItem label='카카오톡' state='연결됨' />
       <AccountMenuItem label='로그아웃' iconType='로그아웃' handleClick={handleLogout} />
       <AccountMenuItem label='탈퇴하기' iconType='탈퇴' handleClick={handleLeave} />
