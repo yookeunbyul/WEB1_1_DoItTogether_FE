f2bfce95d2e649fbf5cc9245fdffd92202cdc438
yook <si89336@naver.com>
2024-11-29 00:02:30 +0900

    💄 Style: css 수정 및 전체버튼 고려하도록 수정

--- src/components/common/tab/PresetTab/PresetTabItem.tsx
+++ src/components/common/tab/PresetTab/PresetTabItem.tsx
@@ -10,9 +10,9 @@ const PresetTabItem: React.FC<PresetTabItemProps> = ({ name, value, icon }) => {
   return (
     <TabsTrigger
       value={value}
-      className='flex gap-2 text-14 data-[state=active]:bg-black01 data-[state=inactive]:bg-white02 data-[state=active]:text-white03'
+      className='flex gap-2 rounded-xl px-4 py-[6px] text-14 data-[state=active]:bg-black01 data-[state=inactive]:bg-white02 data-[state=active]:text-white03'
     >
-      <div className='h-5 w-5 bg-gray03'>{icon}</div>
+      {name !== '전체' && <div className='h-5 w-5 bg-gray03'>{icon}</div>}
       {name}
     </TabsTrigger>
   );
