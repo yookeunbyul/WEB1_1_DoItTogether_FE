a9ea2c856190f217f3f7c9b1694a3f41c3b13416
yook <si89336@naver.com>
2024-11-29 00:06:18 +0900

    💄 Style: step1 페이지 수정

--- src/pages/HouseWorkStepOnePage.tsx
+++ src/pages/HouseWorkStepOnePage.tsx
@@ -16,10 +16,8 @@ interface SelectedTime {
 
 const HouseWorkStepOnePage = () => {
   const navigate = useNavigate();
-  const [houseWork] = useState(null);
-  const [dueDate] = useState<Date | null>(null);
-  // const [houseWork, setHouseWork] = useState(null);
-  // const [dueDate, setDueDate] = useState<Date | null>(null);
+  const [houseWork, setHouseWork] = useState(null);
+  const [dueDate, setDueDate] = useState<Date | null>(null);
   const [isHouseWorkSheetOpen, setHouseWorkSheetOpen] = useState(false);
   const [isDueDateSheetOpen, setDueDateSheetOpen] = useState(false);
   const [time, setTime] = useState<SelectedTime | null>(null);
@@ -43,7 +41,7 @@ const HouseWorkStepOnePage = () => {
   };
 
   return (
-    <div className='flex h-screen flex-col gap-6 px-5'>
+    <div className='flex h-screen flex-col gap-6 px-5 pb-6'>
       <HeaderWithTitle title='새로운 집안일을 추가해보세요(1/2)' handleClick={handleBackClick} />
       <section className='flex flex-1 flex-col gap-6' aria-label='집안일 추가 컨텐츠'>
         {houseWork ? (
