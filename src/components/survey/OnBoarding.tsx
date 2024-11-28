71a6e7fb76a8a8d509d5ab2b807e45d91a280d42
ayoung <a01259@naver.com>
2024-11-28 11:37:50 +0900

    ✨ Feat: 온보딩 페이지 분석 시, 로딩화면 적용

--- src/components/survey/OnBoarding.tsx
+++ src/components/survey/OnBoarding.tsx
@@ -8,6 +8,7 @@ import Step2 from '@/components/survey/steps/Step2';
 import Step3 from '@/components/survey/steps/Step3';
 import Step4 from '@/components/survey/steps/Step4';
 import Step5 from '@/components/survey/steps/Step5';
+import LoadingScreen from '@/components/survey/LoadingScreen/LoadingScreen';
 
 // TODO 질문지 TITLE, QUESTION 하드코딩 하지말고, 상수파일로 관리하고 여기서는 가져와서 사용
 const DUMMY_QUESTION_STEP1 = [
@@ -30,22 +31,34 @@ interface OnBoardingProps {}
 
 const OnBoarding: React.FC<OnBoardingProps> = ({}) => {
   const [step, setStep] = useState(1);
-  const [answer, setAnswer] = useState<string>('');
-  const [result, setResult] = useState<string[]>([]);
-  const [username, setUsername] = useState<string>('홍길동'); // 수정
+  const [answer, setAnswer] = useState<string>(''); // 사용자 답변
+  const [isCompleted, setIsCompleted] = useState<boolean>(false); // 분석완료 여부
+  const [result, setResult] = useState<string[]>([]); // 분석 결과
+  const [username, setUsername] = useState<string>('사용자'); // 사용자명
+  const [loading, setLoading] = useState<boolean>(false);
   const navigate = useNavigate();
 
   const setNextStep = () => {
     if (step === 4) {
+      setLoading(true);
+      setIsCompleted(false);
+
       // API CALL
-      const DUMMY_RESULT = [
-        '규칙적으로 청소',
-        '나 자신을 위해',
-        '청소 각오는 만땅!',
-        '화장실 청소 좋아!',
-      ];
-      // API 데이터 저장
-      setResult(DUMMY_RESULT);
+      setTimeout(() => {
+        const DUMMY_RESULT = [
+          '규칙적으로 청소',
+          '나 자신을 위해',
+          '청소 각오는 만땅!',
+          '화장실 청소 좋아!',
+        ];
+        // API 데이터 저장
+        setResult(DUMMY_RESULT);
+        setIsCompleted(true);
+
+        setTimeout(() => {
+          setLoading(false);
+        }, 1000);
+      }, 1000); // 1초 동안 메세지 표시
     }
     if (step === 5) {
       navigate('/group-select');
@@ -69,40 +82,48 @@ const OnBoarding: React.FC<OnBoardingProps> = ({}) => {
 
       <Progress value={(step / 5) * 100} className='mb-8' />
       <div className='flex h-screen flex-col gap-8 px-5'>
-        {/* TODO STEP 동일구조라서 hook 으로 사용 고려 */}
-        {step === 1 && (
-          <Step1
-            title={`집안일 청소는\n얼마나 자주 하세요?`}
-            questions={DUMMY_QUESTION_STEP1}
-            handleAnswer={setAnswer}
-          />
+        {loading ? (
+          <LoadingScreen username={username} isCompleted={isCompleted} />
+        ) : (
+          <>
+            {/* TODO STEP 동일구조라서 hook 으로 사용 고려 */}
+            {step === 1 && (
+              <Step1
+                title={`집안일 청소는\n얼마나 자주 하세요?`}
+                questions={DUMMY_QUESTION_STEP1}
+                handleAnswer={setAnswer}
+              />
+            )}
+            {step === 2 && (
+              <Step2
+                title={`주로 어떤 이유로\n청소를 하시나요?`}
+                questions={DUMMY_QUESTION_STEP2}
+                handleAnswer={setAnswer}
+              />
+            )}
+            {step === 3 && (
+              <Step3
+                title={`청소할 때 가장 어려운 점은\n무엇인가요?`}
+                questions={DUMMY_QUESTION_STEP3}
+                handleAnswer={setAnswer}
+              />
+            )}
+            {step === 4 && (
+              <Step4
+                title={`청소할 때 최우선으로\n신경 쓰는 공간을 알려주세요!`}
+                questions={DUMMY_QUESTION_STEP4}
+                handleAnswer={setAnswer}
+              />
+            )}
+            {step === 5 && <Step5 title={`${username}님의 청소성향은`} results={result} />}
+          </>
         )}
-        {step === 2 && (
-          <Step2
-            title={`주로 어떤 이유로\n청소를 하시나요?`}
-            questions={DUMMY_QUESTION_STEP2}
-            handleAnswer={setAnswer}
-          />
-        )}
-        {step === 3 && (
-          <Step3
-            title={`청소할 때 가장 어려운 점은\n무엇인가요?`}
-            questions={DUMMY_QUESTION_STEP3}
-            handleAnswer={setAnswer}
-          />
-        )}
-        {step === 4 && (
-          <Step4
-            title={`청소할 때 최우선으로\n신경 쓰는 공간을 알려주세요!`}
-            questions={DUMMY_QUESTION_STEP4}
-            handleAnswer={setAnswer}
-          />
-        )}
-        {step === 5 && <Step5 title={`${username}님의 청소성향은`} results={result} />}
 
-        <Button size={'large'} onClick={setNextStep}>
-          {step === 5 ? '확인' : '다음'}
-        </Button>
+        {!loading && (
+          <Button size={'large'} onClick={setNextStep}>
+            {step === 5 ? '확인' : '다음'}
+          </Button>
+        )}
       </div>
     </div>
   );
