1ece5c6af2f2b52056664e41d9b6ea6262c3e8bb
Eugeeun <jack603@naver.com>
2024-11-29 00:05:37 +0900

    ✨ Feat: 그룹 선택 바텀시트 전역 상태 관리

--- src/store/useHomePageStore.ts
+++ src/store/useHomePageStore.ts
@@ -3,11 +3,17 @@ import { create } from 'zustand';
 interface HomePageState {
   groupName: string;
   setGroupName: (groupName: string) => void;
+
+  isGroupSelectSheetOpen: boolean;
+  setIsGroupSelectSheetOpen: (isOpen: boolean) => void;
 }
 
 const useHomePageStore = create<HomePageState>(set => ({
   groupName: '기본 그룹명',
   setGroupName: name => set({ groupName: name }),
+
+  isGroupSelectSheetOpen: false,
+  setIsGroupSelectSheetOpen: isOpen => set({ isGroupSelectSheetOpen: isOpen }),
 }));
 
 export default useHomePageStore;
