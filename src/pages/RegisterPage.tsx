2b4f785ca5b6d6ac52fa39402abc466aeca21e56
Commeci <cjtkemfldj@naver.com>
2024-11-28 14:15:40 +0900

    🐛 Fix: Input handle props 수정

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -3,10 +3,12 @@ import InputWithLabel from '@/components/common/input/InputWithLabel';
 import ProfileImg from '@/components/common/profile/ProfileImg';
 import TitleCenter from '@/components/common/title/TitleCenter';
 import RegisterNotice from '@/components/register/RegisterNotice';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const RegisterPage = () => {
   const navigate = useNavigate();
+  const [name, setName] = useState('');
 
   const handleSubmitButton = () => {
     navigate('/survey-intro');
@@ -16,7 +18,13 @@ const RegisterPage = () => {
       <TitleCenter title={`사용하실 닉네임과\n프로필을 설정해주세요`} />
       <ProfileImg />
       <div className='flex w-full flex-col gap-4'>
-        <InputWithLabel label='이름' placeholder='이름을 입력해주세요' disabled={false} />
+        <InputWithLabel
+          label='이름'
+          value={name}
+          handleChange={setName}
+          placeholder='이름을 입력해주세요'
+          disabled={false}
+        />
         <RegisterNotice />
       </div>
       <Button label='확인' variant='full' size='large' handleClick={handleSubmitButton} />
