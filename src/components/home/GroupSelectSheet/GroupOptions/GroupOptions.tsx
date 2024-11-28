0fb3509a52746d220f4f1374aa89c8d17cb08c41
Eugeeun <jack603@naver.com>
2024-11-28 23:42:17 +0900

    ✨ Feat: 그룹들을 props로 전달

--- src/components/home/GroupSelectSheet/GroupOptions/GroupOptions.tsx
+++ src/components/home/GroupSelectSheet/GroupOptions/GroupOptions.tsx
@@ -1,7 +1,10 @@
+import React from 'react';
 import GroupOption from '@/components/home/GroupSelectSheet/GroupOptions/GroupOption';
+interface GroupOptionsProps {
+  groups: Array<string>;
+}
 
-const GroupOptions = () => {
-  const groups = ['우리집', '회사'];
+const GroupOptions: React.FC<GroupOptionsProps> = ({ groups }) => {
   return (
     <ul className='flex flex-col gap-y-6 px-5 pb-14 pt-8'>
       {groups.map(group => (
