7f4206da77b76084a7ff3d85bdb5b88cc7056bbf
Commeci <cjtkemfldj@naver.com>
2024-11-29 10:53:13 +0900

    ✨ Feat: 현재, 이전, 다음 달력 정보 처리, props타입 수정

--- src/components/statistics/monthly/MonthlyGrass/MonthlyGrass.tsx
+++ src/components/statistics/monthly/MonthlyGrass/MonthlyGrass.tsx
@@ -6,19 +6,26 @@ enum CompletionStatus {
   NO = 'no',
 }
 
-interface CompletionData {
+interface DailyTask {
   date: string;
+  totalTasks: number;
+  completedTasks: number;
   status: CompletionStatus;
 }
 
 interface MonthlyGrassProps {
-  completionData: CompletionData[];
+  completionData: { [key: string]: DailyTask };
+  onMonthChange: (monthKey: string) => void;
 }
 
-const MonthlyGrass: React.FC<MonthlyGrassProps> = ({ completionData }) => {
+const MonthlyGrass: React.FC<MonthlyGrassProps> = ({ completionData, onMonthChange }) => {
+  const today = new Date();
+  const firstDayCurrentMonth = new Date(today.getFullYear(), today.getMonth(), 1);
+  const lastDayPreviousMonth = new Date(firstDayCurrentMonth.getTime() - 1);
+
   const getStatus = (date: Date): CompletionStatus => {
     const dateString = date.toLocaleDateString('en-CA');
-    const dayData = completionData.find(data => data.date === dateString);
+    const dayData = completionData[dateString];
     return dayData?.status || CompletionStatus.NO;
   };
 
@@ -34,13 +41,26 @@ const MonthlyGrass: React.FC<MonthlyGrassProps> = ({ completionData }) => {
     }
   };
 
+  // TODO: 나중에 여기서 api 호출 -> 현재 달 기준으로 전과 후
+  const handleMonthChange = ({ activeStartDate }: { activeStartDate: Date | null }) => {
+    if (activeStartDate) {
+      const year = activeStartDate.getFullYear();
+      const month = String(activeStartDate.getMonth() + 1).padStart(2, '0');
+      const monthKey = `${year}-${month}`;
+      onMonthChange(monthKey);
+    }
+  };
+
   return (
     <Calendar
+      defaultActiveStartDate={lastDayPreviousMonth}
+      maxDate={lastDayPreviousMonth}
       tileClassName={getTileClassName}
       view='month'
       locale='ko'
       minDetail='month'
       maxDetail='month'
+      onActiveStartDateChange={handleMonthChange}
       navigationLabel={({ date }) => `${date.getFullYear()}년 ${date.getMonth() + 1}월`}
       formatDay={(_locale, date) => date.getDate().toString()}
     />
