20606a8abcdfb62cfc04540b6f4697f2b04721c0
ayoung <a01259@naver.com>
2024-11-29 19:58:14 +0900

    ✨ Feat: 상수파일 모듈화

---
+++ src/constants/index.ts
@@ -0,0 +1,10 @@
+export { Category } from '@/constants/Category';
+export { PresetDefault } from '@/constants/PresetDefault';
+export { PresetTabName } from '@/constants/TabName';
+export {
+  DUMMY_QUESTION_STEP1,
+  DUMMY_QUESTION_STEP2,
+  DUMMY_QUESTION_STEP3,
+  DUMMY_QUESTION_STEP4,
+  DUMMY_RESULT,
+} from '@/constants/onBoarding';
