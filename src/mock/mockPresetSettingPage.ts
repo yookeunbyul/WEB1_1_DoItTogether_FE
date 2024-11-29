abd0ca678626adc9926ef1ed5edca81de455e171
ayoung <a01259@naver.com>
2024-11-29 20:00:23 +0900

    ➕ Add: 목업 더미데이터 파일 추가

---
+++ src/mock/mockPresetSettingPage.ts
@@ -0,0 +1,68 @@
+export const mockData = {
+  userData: [
+    {
+      category: '거실',
+      items: [{ id: 1, description: '매일 아침 화장실 청소하기' }],
+    },
+    {
+      category: '부엌',
+      items: [{ id: 2, description: '아침 식사 후 설거지하기' }],
+    },
+    {
+      category: '1번 방',
+      items: [{ id: 3, description: '1번 방 바닥 청소' }],
+    },
+    {
+      category: '2번 방',
+      items: [{ id: 4, description: '2번 방 옷장 정리' }],
+    },
+    {
+      category: '3번 방',
+      items: [{ id: 5, description: '3번 방 나가기' }],
+    },
+  ],
+  presetData: [
+    {
+      category: '거실',
+      items: [
+        { id: 1, description: '매일 아침 화장실 청소하기' },
+        { id: 2, description: '거실 바닥 청소하기' },
+        { id: 3, description: '거실 청소기 돌리기' },
+        { id: 4, description: '거실 청소기 돌리기' },
+        { id: 5, description: '거실 청소기 돌리기' },
+        { id: 6, description: '거실 청소기 돌리기' },
+        { id: 7, description: '거실 청소기 돌리기' },
+        { id: 8, description: '거실 청소기 돌리기' },
+        { id: 9, description: '거실 청소기 돌리기' },
+        { id: 10, description: '거실 청소기 돌리기' },
+        { id: 11, description: '거실 청소기 돌리기' },
+        { id: 12, description: '거실 청소기 돌리기' },
+        { id: 13, description: '거실 청소기 돌리기' },
+        { id: 14, description: '거실 청소기 돌리기' },
+        { id: 15, description: '거실 청소기 돌리기' },
+        { id: 16, description: '거실 청소기 돌리기' },
+        { id: 17, description: '거실 청소기 돌리기' },
+      ],
+    },
+    {
+      category: '부엌',
+      items: [
+        { id: 4, description: '아침 식사 후 설거지하기' },
+        { id: 5, description: '저녁 식사 후 설거지하기' },
+        { id: 6, description: '오븐 청소' },
+      ],
+    },
+    {
+      category: '1번 방',
+      items: [{ id: 7, description: '1번 방 청소' }],
+    },
+    {
+      category: '2번 방',
+      items: [{ id: 8, description: '2번 방 청소' }],
+    },
+    {
+      category: '3번 방',
+      items: [{ id: 9, description: '3번 방 청소' }],
+    },
+  ],
+};
