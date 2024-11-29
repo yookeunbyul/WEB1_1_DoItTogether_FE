61efc30568e8add496c8159ebf819350bd8e157f
Eugeeun <jack603@naver.com>
2024-11-29 14:24:05 +0900

    ✨ Feat: 리스트 액션 버튼 클리시 완료 처리

--- src/components/home/HouseworkList/HouseworkList.tsx
+++ src/components/home/HouseworkList/HouseworkList.tsx
@@ -1,17 +1,36 @@
 import React from 'react';
-import HouseworkListItem, {
-  HouseworkListItemProps,
-} from '@/components/home/HouseworkList/HouseworkListItem/HouseworkListItem';
+import HouseworkListItem from '@/components/home/HouseworkList/HouseworkListItem/HouseworkListItem';
 
 export interface HouseworkListProps {
-  items: HouseworkListItemProps[];
+  items: {
+    id: number;
+    actionStatus: string;
+    listTitle: string;
+    charger: string;
+    time: string;
+    category: string;
+  }[];
+  handleAction: (id: number) => void;
+  handleEdit: () => void;
+  handleDelete: () => void;
 }
 
-const HouseworkList: React.FC<HouseworkListProps> = ({ items }) => {
+const HouseworkList: React.FC<HouseworkListProps> = ({
+  items,
+  handleAction,
+  handleEdit,
+  handleDelete,
+}) => {
   return (
     <div className='flex max-w flex-col gap-2 p-5'>
       {items.map(item => (
-        <HouseworkListItem key={item.id} {...item} />
+        <HouseworkListItem
+          key={item.id}
+          {...item}
+          handleAction={handleAction}
+          handleEdit={handleEdit}
+          handleDelete={handleDelete}
+        />
       ))}
     </div>
   );
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,192 +5,43 @@ import HouseworkList from '@/components/home/HouseworkList/HouseworkList';
 import GroupSelectSheet from '@/components/home/GroupSelectSheet/GroupSelectSheet';
 import useHomePageStore from '@/store/useHomePageStore';
 import getWeekText from '@/utils/getWeekText';
-
-export const data = [
-  {
-    id: 1,
-    actionStatus: 'incomplete',
-    handleAction: () => {
-      console.log('Action btn clicked for 바닥 걸레질');
-    },
-    listTitle: '바닥 걸레질',
-    charger: '홍길동',
-    time: '오후 8:00',
-    handleEdit: () => {
-      console.log('Edit triggered for 바닥 걸레질');
-    },
-    handleDelete: () => {
-      console.log('Delete triggered for 바닥 걸레질');
-    },
-    category: '거실',
-  },
-  {
-    id: 2,
-    actionStatus: 'incomplete',
-    handleAction: () => {
-      console.log('Action btn clicked for 주방 청소');
-    },
-    listTitle: '청소',
-    charger: '김철수',
-    time: '오후 9:00',
-    handleEdit: () => {
-      console.log('Edit triggered for 주방 청소');
-    },
-    handleDelete: () => {
-      console.log('Delete triggered for 주방 청소');
-    },
-    category: '주방',
-  },
-  {
-    id: 3,
-    actionStatus: 'complete',
-    handleAction: () => {
-      console.log('Action btn clicked for 세탁기 돌리기');
-    },
-    listTitle: '세탁기 돌리기',
-    charger: '이영희',
-    time: '오후 10:00',
-    handleEdit: () => {
-      console.log('Edit triggered for 세탁기 돌리기');
-    },
-    handleDelete: () => {
-      console.log('Delete triggered for 세탁기 돌리기');
-    },
-    category: '기타',
-  },
-  {
-    id: 4,
-    actionStatus: 'complete',
-    handleAction: () => {
-      console.log('Action btn clicked for 정리 정돈');
-    },
-    listTitle: '정리 정돈',
-    charger: '박지성',
-    time: '오후 7:00',
-    handleEdit: () => {
-      console.log('Edit triggered for 정리 정돈');
-    },
-    handleDelete: () => {
-      console.log('Delete triggered for 정리 정돈');
-    },
-    category: '침실',
-  },
-  {
-    id: 5,
-    actionStatus: 'incomplete',
-    handleAction: () => {
-      console.log('Action btn clicked for 화장실 청소');
-    },
-    listTitle: '화장실 청소',
-    charger: '최민수',
-    time: '오후 6:00',
-    handleEdit: () => {
-      console.log('Edit triggered for 화장실 청소');
-    },
-    handleDelete: () => {
-      console.log('Delete triggered for 화장실 청소');
-    },
-    category: '화장실',
-  },
-  {
-    id: 6,
-    actionStatus: 'complete',
-    handleAction: () => {
-      console.log('Action btn clicked for 정원 가꾸기');
-    },
-    listTitle: '정원 가꾸기',
-    charger: '홍길동',
-    time: '오후 5:00',
-    handleEdit: () => {
-      console.log('Edit triggered for 정원 가꾸기');
-    },
-    handleDelete: () => {
-      console.log('Delete triggered for 정원 가꾸기');
-    },
-    category: '정원',
-  },
-  {
-    id: 7,
-    actionStatus: 'incomplete',
-    handleAction: () => {
-      console.log('Action btn clicked for 쓰레기 버리기');
-    },
-    listTitle: '쓰레기 버리기',
-    charger: '김철수',
-    time: '오후 4:00',
-    handleEdit: () => {
-      console.log('Edit triggered for 쓰레기 버리기');
-    },
-    handleDelete: () => {
-      console.log('Delete triggered for 쓰레기 버리기');
-    },
-    category: '기타',
-  },
-  {
-    id: 8,
-    actionStatus: 'complete',
-    handleAction: () => {
-      console.log('Action btn clicked for 빨래 널기');
-    },
-    listTitle: '빨래 널기',
-    charger: '이영희',
-    time: '오후 3:00',
-    handleEdit: () => {
-      console.log('Edit triggered for 빨래 널기');
-    },
-    handleDelete: () => {
-      console.log('Delete triggered for 빨래 널기');
-    },
-    category: '세탁실',
-  },
-  {
-    id: 9,
-    actionStatus: 'incomplete',
-    handleAction: () => {
-      console.log('Action btn clicked for 청소기 돌리기');
-    },
-    listTitle: '청소기 돌리기',
-    charger: '박지성',
-    time: '오후 2:00',
-    handleEdit: () => {
-      console.log('Edit triggered for 청소기 돌리기');
-    },
-    handleDelete: () => {
-      console.log('Delete triggered for 청소기 돌리기');
-    },
-    category: '거실',
-  },
-  {
-    id: 10,
-    actionStatus: 'complete',
-    handleAction: () => {
-      console.log('Action btn clicked for 정리 정돈');
-    },
-    listTitle: '정리 정돈',
-    charger: '최민수',
-    time: '오후 1:00',
-    handleEdit: () => {
-      console.log('Edit triggered for 정리 정돈');
-    },
-    handleDelete: () => {
-      console.log('Delete triggered for 정리 정돈');
-    },
-    category: '침실',
-  },
-];
+import { DUMMY_HOUSEWORKS } from '@/mock/mockHomePage';
 
 const HomePage: React.FC = () => {
   const [activeTab, setActiveTab] = useState<string>('전체');
+  const [houseworkList, setHouseworkList] = useState(DUMMY_HOUSEWORKS);
   const { setWeekText } = useHomePageStore();
 
   const chargers = [
     { name: '전체' },
-    ...Array.from(new Set(data.map(item => item.charger))).map(charger => ({ name: charger })),
+    ...Array.from(new Set(DUMMY_HOUSEWORKS.map(item => item.charger))).map(charger => ({
+      name: charger,
+    })),
   ];
 
   useEffect(() => {
     setWeekText(getWeekText(new Date()));
   }, []);
+
+  const handleAction = (id: number) => {
+    setHouseworkList(
+      houseworkList.map(housework => {
+        if (housework.id === id) {
+          return {
+            ...housework,
+            actionStatus: housework.actionStatus === 'complete' ? 'incomplete' : 'complete',
+          };
+        }
+        return housework;
+      })
+    );
+  };
+  const handleEdit = () => {
+    console.log('edit');
+  };
+  const handleDelete = () => {
+    console.log('delete');
+  };
 
   return (
     <div>
@@ -201,7 +52,10 @@ const HomePage: React.FC = () => {
         chargers={chargers}
       />
       <HouseworkList
-        items={data.filter(item => item.charger === activeTab || activeTab === '전체')}
+        items={houseworkList.filter(item => item.charger === activeTab || activeTab === '전체')}
+        handleAction={handleAction}
+        handleEdit={handleEdit}
+        handleDelete={handleDelete}
       />
       <GroupSelectSheet />
     </div>
