ee2cb05ef281b26eddf224bebc7a315fc3221cca
Eugeeun <jack603@naver.com>
2024-11-30 17:39:44 +0900

    ✨ Feat: 바텀 네비게이션 라우팅

--- src/components/common/bottomNav/BottomNav.tsx
+++ src/components/common/bottomNav/BottomNav.tsx
@@ -1,9 +1,11 @@
 import { useNavigate } from 'react-router-dom';
 import AddHouseWorkBtn from './AddHouseWorkBtn/AddHouseWorkBtn';
 import BottomNavBtn from './BottomNavBtn/BottomNavBtn';
+import useHomePageStore from '@/store/useHomePageStore';
 
 const BottomNav = () => {
   const navigate = useNavigate();
+  const { currentGroup } = useHomePageStore();
 
   const navItems = [
     { name: '홈', icon: null },
@@ -21,23 +23,23 @@ const BottomNav = () => {
       <BottomNavBtn
         icon={navItems[0].icon}
         name={navItems[0].name}
-        handleClick={() => handleClick('/main')}
+        handleClick={() => handleClick(`/main/${currentGroup.channelId}`)}
       />
       <BottomNavBtn
         icon={navItems[1].icon}
         name={navItems[1].name}
-        handleClick={() => handleClick('/main/statistics/weekly')}
+        handleClick={() => handleClick(`/main/${currentGroup.channelId}/statistics/weekly`)}
       />
       <AddHouseWorkBtn handleClick={() => handleClick('/add-housework/step1')} />
       <BottomNavBtn
         icon={navItems[2].icon}
         name={navItems[2].name}
-        handleClick={() => handleClick('/main/group-setting')}
+        handleClick={() => handleClick(`/main/${currentGroup.channelId}/group-setting`)}
       />
       <BottomNavBtn
         icon={navItems[3].icon}
         name={navItems[3].name}
-        handleClick={() => handleClick('/main/my-page')}
+        handleClick={() => handleClick(`/main/${currentGroup.channelId}/my-page`)}
       />
     </div>
   );
