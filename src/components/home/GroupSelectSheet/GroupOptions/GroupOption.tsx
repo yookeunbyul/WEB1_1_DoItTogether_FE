e716151d8263fc748d0b5bbd7f79e07e6c995799
Eugeeun <jack603@naver.com>
2024-11-30 16:23:40 +0900

    ✨ Feat: 그룹명에서 그룹으로 수정

--- src/components/home/GroupSelectSheet/GroupOptions/GroupOption.tsx
+++ src/components/home/GroupSelectSheet/GroupOptions/GroupOption.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
 import useHomePageStore from '@/store/useHomePageStore';
+import { Group } from '@/types/apis/groupApi';
+import { useNavigate } from 'react-router-dom';
 
 interface GroupOptionProps {
-  /** 그룹(방) 이름 */
-  groupName: string;
+  /** 그룹(방)  */
+  group: Group;
   /** 선택 여부 */
   isSelected?: boolean;
 }
 
-const GroupOption: React.FC<GroupOptionProps> = ({ groupName, isSelected }) => {
-  const { setGroupName, setIsGroupSelectSheetOpen } = useHomePageStore();
+const GroupOption: React.FC<GroupOptionProps> = ({ group, isSelected }) => {
+  const { setCurrentGroup, setIsGroupSelectSheetOpen } = useHomePageStore();
+  const navigate = useNavigate();
 
-  const handleClick = (group: string) => {
-    setGroupName(group);
+  const handleClick = (group: Group) => {
+    setCurrentGroup(group);
     setIsGroupSelectSheetOpen(false);
+    navigate(`/main/${group.channelId}`);
   };
 
   return (
-    <li className='flex cursor-pointer items-center gap-x-2' onClick={() => handleClick(groupName)}>
+    <li className='flex cursor-pointer items-center gap-x-2' onClick={() => handleClick(group)}>
       <div className={`h-6 w-6 rounded-md ${isSelected ? 'bg-black01' : 'bg-gray02'}`}></div>
-      <div className={`text-14 ${isSelected ? 'text-black01' : 'text-gray02'}`}>{groupName}</div>
+      <div className={`text-14 ${isSelected ? 'text-black01' : 'text-gray02'}`}>{group.name}</div>
     </li>
   );
 };
