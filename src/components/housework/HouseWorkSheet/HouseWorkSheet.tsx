7063be561fabdf1e6fc91c2bb541cc86e2f07207
ayoung <a01259@naver.com>
2024-11-30 18:01:06 +0900

    ✨ Feat: 집안일추가 바텀시트 (전체, 특정) 카테고리 리스트 조회 기능 구현

--- src/components/housework/HouseWorkSheet/HouseWorkSheet.tsx
+++ src/components/housework/HouseWorkSheet/HouseWorkSheet.tsx
@@ -1,10 +1,12 @@
+import { useState } from 'react';
 import BottomSheet from '@/components/common/bottomSheet/BottomSheet';
 import Button from '@/components/common/button/Button/Button';
 import PresetTab from '@/components/common/tab/PresetTab/PresetTab';
 import Tab from '@/components/common/tab/Tab/Tab';
-import { mockData as housework } from '@/mock/mockPresetSettingPage';
 import useAddHouseWorkStore from '@/store/useAddHouseWorkStore';
-import { useState } from 'react';
+import { mockData } from '@/mock/mockPresetSettingPage';
+import { PresetDefault, PresetTabName } from '@/constants';
+import { convertTabNameToChargers } from '@/utils/ConvertUtils';
 
 interface HouseWorkSheetProps {
   /** 바텀시트 오픈 여부 */
@@ -14,15 +16,11 @@ interface HouseWorkSheetProps {
 }
 
 const HouseWorkSheet: React.FC<HouseWorkSheetProps> = ({ isOpen, setOpen }) => {
-  const [activeTab, setActiveTab] = useState<string>('사용자 정의');
+  const [activeTab, setActiveTab] = useState<string>(PresetTabName.USER_DATA);
   const [selectedHouseWork, setSelectedHouseWork] = useState<string | null>(null);
   const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
   const [selectedItem, setSelectedItem] = useState<number | null>(null);
   const { setTask, setCategory } = useAddHouseWorkStore();
-
-  const chargers = Object.keys(housework).map(key => ({
-    name: key === 'userData' ? '사용자 정의' : '프리셋',
-  }));
 
   const handleDoneClick = () => {
     if (selectedHouseWork) {
@@ -47,15 +45,23 @@ const HouseWorkSheet: React.FC<HouseWorkSheetProps> = ({ isOpen, setOpen }) => {
     }
   };
 
+  const getPresetData = () => {
+    return activeTab === PresetTabName.USER_DATA ? mockData.userData : PresetDefault;
+  };
+
   return (
     <BottomSheet isOpen={isOpen} setOpen={setOpen} title='집안일 선택'>
       <div className='flex min-h-96 flex-col gap-y-6 pb-6'>
         <section aria-label='집안일 할당 바텀 시트' className='flex flex-1 flex-col gap-6'>
           <div>
-            <Tab activeTab={activeTab} handleSetActiveTab={setActiveTab} chargers={chargers} />
+            <Tab
+              activeTab={activeTab}
+              handleSetActiveTab={setActiveTab}
+              chargers={convertTabNameToChargers(PresetTabName)}
+            />
           </div>
           <PresetTab
-            data={activeTab === '사용자 정의' ? housework.userData : housework.presetData}
+            data={getPresetData()}
             isBottomSheet={true}
             handleClick={handleClick}
             selectedItem={selectedItem}
