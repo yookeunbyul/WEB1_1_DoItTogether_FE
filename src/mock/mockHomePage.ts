10c6b4b9d0162e7be49112cc14f449b41b1a1179
Eugeeun <jack603@naver.com>
2024-12-01 17:04:22 +0900

    🔧 Wrench: 더미 데이터 수정

--- src/mock/mockHomePage.ts
+++ src/mock/mockHomePage.ts
@@ -1,28 +1,54 @@
+import { Housework } from '@/types/apis/houseworkApi';
+
 export const GROUP_OPTIONS = ['우리집', '회사', '학교'];
 
-export const DUMMY_HOUSEWORKS = [
+export const DUMMY_HOUSEWORKS: Housework[] = [
   {
-    id: 1,
-    actionStatus: 'incomplete',
-    listTitle: '바닥 걸레질',
-    charger: '홍길동',
-    time: '오후 8:00',
+    houseworkId: 1,
     category: '거실',
+    task: '바닥 걸레질',
+    startDate: '2024-12-01', // 예시 날짜, 필요에 맞게 수정
+    startTime: '오후 8:00',
+    isAllDay: false, // 하루 종일 여부, 필요에 따라 수정
+    assignee: '고종원', // 변경된 이름
+    userId: 1, // userId는 실제 데이터에 맞게 수정
+    assigneeId: 1, // assigneeId는 실제 데이터에 맞게 수정
+    status: 'INCOMPLETE',
   },
   {
-    id: 2,
-    actionStatus: 'incomplete',
-    listTitle: '청소',
-    charger: '김철수',
-    time: '오후 9:00',
+    houseworkId: 2,
     category: '주방',
+    task: '청소',
+    startDate: '2024-12-01', // 예시 날짜, 필요에 맞게 수정
+    startTime: '오후 9:00',
+    isAllDay: false, // 하루 종일 여부, 필요에 따라 수정
+    assignee: '이가은', // 변경된 이름
+    userId: 2, // userId는 실제 데이터에 맞게 수정
+    assigneeId: 2, // assigneeId는 실제 데이터에 맞게 수정
+    status: 'INCOMPLETE',
   },
   {
-    id: 3,
-    actionStatus: 'complete',
-    listTitle: '세탁기 돌리기',
-    charger: '이영희',
-    time: '오후 10:00',
+    houseworkId: 3,
     category: '기타',
+    task: '세탁기 돌리기',
+    startDate: '2024-12-01', // 예시 날짜, 필요에 맞게 수정
+    startTime: '오후 10:00',
+    isAllDay: false, // 하루 종일 여부, 필요에 따라 수정
+    assignee: '육은별', // 변경된 이름
+    userId: 3, // userId는 실제 데이터에 맞게 수정
+    assigneeId: 3, // assigneeId는 실제 데이터에 맞게 수정
+    status: 'COMPLETE',
+  },
+  {
+    houseworkId: 4, // 새 작업 추가
+    category: '거실',
+    task: '청소기 돌리기',
+    startDate: '2024-12-01', // 예시 날짜, 필요에 맞게 수정
+    startTime: '오후 11:00',
+    isAllDay: false, // 하루 종일 여부, 필요에 따라 수정
+    assignee: '박아영', // 변경된 이름
+    userId: 4, // userId는 실제 데이터에 맞게 수정
+    assigneeId: 4, // assigneeId는 실제 데이터에 맞게 수정
+    status: 'INCOMPLETE',
   },
 ];
