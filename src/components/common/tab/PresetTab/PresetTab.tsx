d0e1904940d833a86bb3896900b48e01e3107d8b
ayoung <a01259@naver.com>
2024-12-01 23:53:30 +0900

    ✨ Feat: api req 에 맞게 구조, 네이밍 변경

--- src/components/common/tab/PresetTab/PresetTab.tsx
+++ src/components/common/tab/PresetTab/PresetTab.tsx
@@ -4,17 +4,22 @@ import { Tabs, TabsContent, TabsList } from '@/components/common/ui/tabs';
 import { Category as PresetCategory } from '@/constants/category';
 
 interface PresetItem {
-  id: number;
-  description: string;
+  // 프리셋 아이템 아이디
+  presetItemId: number;
+  // 프리셋 아이템 이름
+  name: string;
 }
-
-interface PresetData {
+interface PresetList {
+  // 프리셋 카테고리 아이디
+  presetCategoryId: number;
+  // 프리셋 카테고리 이름
   category: string;
-  items: PresetItem[];
+  // 프리셋 아이템 리스트
+  presetItemList: Array<PresetItem>;
 }
 
 interface PresetTabProps {
-  data: PresetData[];
+  presetData: PresetList[];
   isPresetSettingCustom?: boolean;
   deleteButtonStates?: Record<number, boolean>;
   handleSettingClick?: (itemId: number) => void;
@@ -25,7 +30,7 @@ interface PresetTabProps {
 }
 
 const PresetTab: React.FC<PresetTabProps> = ({
-  data,
+  presetData,
   isPresetSettingCustom = false,
   deleteButtonStates = {},
   handleSettingClick,
@@ -36,8 +41,8 @@ const PresetTab: React.FC<PresetTabProps> = ({
 }) => {
   const allPresetData = {
     category: PresetCategory.ALL,
-    items: data.flatMap(category =>
-      category.items.map(item => ({ ...item, category: category.category }))
+    items: presetData.flatMap(categoryList =>
+      categoryList.presetItemList.map(item => ({ ...item, category: categoryList.category }))
     ),
   };
 
@@ -45,8 +50,13 @@ const PresetTab: React.FC<PresetTabProps> = ({
     <Tabs defaultValue={PresetCategory.ALL}>
       <TabsList className='flex w-full justify-start gap-4 overflow-x-auto overflow-y-hidden bg-white03 p-0 px-5 no-scrollbar'>
         <PresetTabItem name={allPresetData.category} value={allPresetData.category} icon='' />
-        {data.map((tab, index) => (
-          <PresetTabItem key={index} name={tab.category} value={tab.category} icon='' />
+        {presetData.map(categoryList => (
+          <PresetTabItem
+            key={categoryList.presetCategoryId}
+            name={categoryList.category}
+            value={categoryList.category}
+            icon=''
+          />
         ))}
       </TabsList>
       <TabsContent
@@ -55,44 +65,50 @@ const PresetTab: React.FC<PresetTabProps> = ({
         className={`${isBottomSheet ? 'h-[250px]' : 'h-auto'} overflow-y-auto no-scrollbar`}
       >
         {allPresetData.items.map(item => (
-          <div key={item.id}>
+          <div key={item.presetItemId}>
             <PresetItem
               category={item.category}
-              housework={item.description}
+              housework={item.name}
               handleSelectClick={() =>
-                handleClick && handleClick(item.id, item.description, item.category)
+                handleClick && handleClick(item.presetItemId, item.name, item.category)
               }
               isBottomSheet={isBottomSheet}
               isPresetSettingCustom={isPresetSettingCustom}
-              isShowDeleteBtn={deleteButtonStates[item.id]} //각 아이템의 boolean값이 들어간다.
-              handleSettingClick={handleSettingClick && (() => handleSettingClick(item.id))}
-              handleDeleteClick={handleDeleteClick && (() => handleDeleteClick(item.id))}
-              isSelected={selectedItem === item.id}
+              isShowDeleteBtn={deleteButtonStates[item.presetItemId]} //각 아이템의 boolean값이 들어간다.
+              handleSettingClick={
+                handleSettingClick && (() => handleSettingClick(item.presetItemId))
+              }
+              handleDeleteClick={handleDeleteClick && (() => handleDeleteClick(item.presetItemId))}
+              isSelected={selectedItem === item.presetItemId}
             />
           </div>
         ))}
       </TabsContent>
 
-      {data.map(tabData => (
+      {presetData.map(categoryList => (
         <TabsContent
-          key={tabData.category}
-          value={tabData.category}
+          key={categoryList.presetCategoryId}
+          value={categoryList.category}
           className={`${isBottomSheet ? 'h-[250px]' : 'h-auto'} overflow-y-auto no-scrollbar`}
         >
-          {tabData.items.map(item => (
-            <div key={item.id}>
+          {categoryList.presetItemList.map(item => (
+            <div key={item.presetItemId}>
               <PresetItem
-                category={tabData.category}
-                housework={item.description}
+                category={categoryList.category}
+                housework={item.name}
                 handleSelectClick={() =>
-                  handleClick && handleClick(item.id, item.description, tabData.category)
+                  handleClick && handleClick(item.presetItemId, item.name, categoryList.category)
                 }
                 isBottomSheet={isBottomSheet}
                 isPresetSettingCustom={isPresetSettingCustom}
-                isShowDeleteBtn={deleteButtonStates[item.id]} //각 아이템의 boolean값이 들어간다.
-                handleSettingClick={handleSettingClick && (() => handleSettingClick(item.id))}
-                handleDeleteClick={handleDeleteClick && (() => handleDeleteClick(item.id))}
-                isSelected={selectedItem === item.id}
+                isShowDeleteBtn={deleteButtonStates[item.presetItemId]} //각 아이템의 boolean값이 들어간다.
+                handleSettingClick={
+                  handleSettingClick && (() => handleSettingClick(item.presetItemId))
+                }
+                handleDeleteClick={
+                  handleDeleteClick && (() => handleDeleteClick(item.presetItemId))
+                }
+                isSelected={selectedItem === item.presetItemId}
               />
             </div>
           ))}
