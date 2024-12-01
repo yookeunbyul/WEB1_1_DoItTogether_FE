ae4b2304f37057005e60f3066aa1d023c8d9f857
ayoung <a01259@naver.com>
2024-12-01 17:40:02 +0900

    🐛 Fix: 프리셋 카테고리 생성 api 호출 로직 제거

--- src/services/setting/preset/deleteCategory.ts
+++ src/services/setting/preset/deleteCategory.ts
@@ -1,6 +1,7 @@
 import { axiosInstance } from '@/services/axiosInstance';
 import { DeletePresetCategoryReq, DeletePresetCategoryRes } from '@/types/apis/presetApi';
 
+// 현재 호출부 없음
 export const deleteCategory = async (data: DeletePresetCategoryReq) => {
   try {
     const response = await axiosInstance.delete<DeletePresetCategoryRes>(
