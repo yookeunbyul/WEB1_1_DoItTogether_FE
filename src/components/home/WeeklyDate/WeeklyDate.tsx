f8a2a0c77b3f7db0071b57151d585cf742024261
Eugeeun <jack603@naver.com>
2024-12-02 16:24:38 +0900

    💄 Style: 메인페이지 데이트 피커 스타일 반영

--- src/components/home/WeeklyDate/WeeklyDate.tsx
+++ src/components/home/WeeklyDate/WeeklyDate.tsx
@@ -57,7 +57,7 @@ const WeeklyDate = () => {
       <CarouselContent>
         {[...Array(3)].map((_, i) => (
           <CarouselItem key={i}>
-            <div className='flex touch-none items-center justify-between bg-white03 px-5 py-2'>
+            <div className='bg-white flex touch-none items-center justify-center gap-3 px-5 py-2'>
               {currWeek.map((week, idx) => (
                 <DateItem
                   key={idx}
