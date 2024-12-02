114427f6e7f9dec8ecc5450bccd3a5ce64b4bf7b
Eugeeun <jack603@naver.com>
2024-12-03 01:02:24 +0900

    ➕ Add: 집안일 카테고리 아이콘 그룹 추가

---
+++ src/components/common/icon/KitchenIconGroup.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+interface KitchenIconGroupProps {}
+
+const KitchenIconGroup: React.FC<KitchenIconGroupProps> = ({}) => {
+  return (
+    <svg
+      width='155'
+      height='80'
+      viewBox='0 0 155 80'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <g opacity='0.5'>
+        <path
+          d='M172.893 58.4316C176.774 51.7098 174.471 43.1147 167.749 39.2339L112.582 7.38327C105.86 3.50244 97.2652 5.80549 93.3844 12.5273L76.3869 41.9678L155.896 87.8722L172.893 58.4316Z'
+          fill='#1FCFBA'
+        />
+        <path
+          d='M95.4458 13.4121C99.3266 6.69029 97.0236 -1.90482 90.3018 -5.78566L75.8577 -14.1249C69.1359 -18.0057 60.5408 -15.7027 56.66 -8.98089L39.1815 21.2927L77.9673 43.6857L95.4458 13.4121Z'
+          fill='url(#paint0_linear_2160_17554)'
+        />
+        <path
+          d='M163.905 57.1111C167.384 51.0846 165.32 43.3786 159.293 39.8993L115.926 14.8615C109.9 11.3821 102.194 13.4469 98.7147 19.4733L85.5794 42.2244L150.77 79.8621L163.905 57.1111Z'
+          fill='#00A18E'
+          fill-opacity='0.7'
+        />
+        <path
+          d='M155.896 87.876L76.3878 41.9716L45.9638 94.6676C42.083 101.389 44.386 109.985 51.1078 113.865L106.275 145.716C112.996 149.597 121.592 147.294 125.472 140.572L155.896 87.876Z'
+          fill='#8DE8D7'
+        />
+      </g>
+      <defs>
+        <linearGradient
+          id='paint0_linear_2160_17554'
+          x1='52.5885'
+          y1='-0.238822'
+          x2='90.6425'
+          y2='21.7317'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop offset='0.5' stop-color='#8DE8D7' />
+          <stop offset='0.947356' stop-color='#1FCFBA' />
+          <stop offset='0.947456' stop-color='#09AF9B' />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
+
+export default KitchenIconGroup;
