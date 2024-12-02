aba88efef03e6211b0f2389f8ae2e441baf9b4c4
Commeci <cjtkemfldj@naver.com>
2024-12-02 14:18:52 +0900

    💄 Style: 스타일 수정, 불러오는 컴포넌트 변경

--- src/pages/MonthlyStatisticsPage.tsx
+++ src/pages/MonthlyStatisticsPage.tsx
@@ -1,5 +1,4 @@
-import CompletionDate from '@/components/statistics/monthly/CompletionDate/CompletionDate';
-import CompletionRate from '@/components/statistics/monthly/CompletionRate/CompletionRate';
+import CompletionDate from '@/components/statistics/monthly/Completion/Completion';
 import MonthlyGoodBad from '@/components/statistics/monthly/MonthlyGoodBad/MonthlyGoodBad';
 import MonthlyGrass from '@/components/statistics/monthly/MonthlyGrass/MonthlyGrass';
 import { monthlyGoodBad } from '@/mock/monthlyGoodBad';
@@ -14,13 +13,29 @@ const MonthlyStatisticsPage = () => {
   };
 
   return (
-    <div className='flex flex-col gap-5'>
+    <div className='flex flex-col gap-4'>
       <MonthlyGrass completionData={monthlyGrassData.dates} onMonthChange={handleMonthChange} />
-      <div className='flex h-8 justify-between'>
-        <CompletionRate rate={monthlyGrassData.monthlyStats[currentMonth]?.completionRate} />
-        <CompletionDate days={monthlyGrassData.monthlyStats[currentMonth]?.completedDays} />
-      </div>
-      <div className='flex gap-3'>
+      <p className='text-gray1 font-label flex items-center gap-3'>
+        이번달에는
+        <p className='flex items-center gap-3'>
+          <span className='text-main flex items-center'>
+            <CompletionDate
+              count={monthlyGrassData.monthlyStats[currentMonth]?.completionRate}
+              color='text-main'
+            />
+            %
+          </span>
+          <span className='text-main flex items-center'>
+            <CompletionDate
+              count={monthlyGrassData.monthlyStats[currentMonth]?.completedDays}
+              color='text-main'
+            />
+            개의
+          </span>
+        </p>
+        작업을 완료했어요!
+      </p>
+      <div className='flex gap-3 py-2'>
         <MonthlyGoodBad type='칭찬' name={monthlyGoodBad.good.member} />
         <MonthlyGoodBad type='찌르기' name={monthlyGoodBad.bad.member} />
       </div>
