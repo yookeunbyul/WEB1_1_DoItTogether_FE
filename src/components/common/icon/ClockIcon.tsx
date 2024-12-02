25b1bc2f52617e700e0748e4e9d3e527d5c9f677
Commeci <cjtkemfldj@naver.com>
2024-12-02 22:18:45 +0900

    ➕ Add: 아이콘 컴포넌트 파일들 추가

---
+++ src/components/common/icon/ClockIcon.tsx
@@ -0,0 +1,23 @@
+const ClockIcon = () => {
+  return (
+    <svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path
+        d='M20.9 12C20.9 16.9153 16.9153 20.9 12 20.9C7.08467 20.9 3.1 16.9153 3.1 12C3.1 7.08467 7.08467 3.1 12 3.1C16.9153 3.1 20.9 7.08467 20.9 12Z'
+        fill='#1FCFBA'
+        stroke='#1FCFBA'
+        stroke-width='2.2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+      />
+      <path
+        d='M12 7V12L15.3333 13.6667'
+        stroke='#FDFDFD'
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+      />
+    </svg>
+  );
+};
+
+export default ClockIcon;
---
+++ src/components/common/icon/KitchenIcon.tsx
@@ -0,0 +1,20 @@
+const KitchenIcon = () => {
+  return (
+    <svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <rect x='7.29883' y='19.9951' width='1.2221' height='1.00508' fill='#8DE8D7' />
+      <rect x='15.0938' y='19.9951' width='1.2221' height='1.00508' fill='#8DE8D7' />
+      <path
+        d='M5.4668 5C5.4668 3.89543 6.36223 3 7.4668 3H16.5321C17.6367 3 18.5321 3.89543 18.5321 5V9.83789H5.4668V5Z'
+        fill='#1FCFBA'
+      />
+      <path
+        d='M5.4668 9.83789H18.5321V18.4973C18.5321 19.6018 17.6367 20.4973 16.5321 20.4973H7.4668C6.36223 20.4973 5.4668 19.6018 5.4668 18.4973V9.83789Z'
+        fill='#8DE8D7'
+      />
+      <rect x='6.55078' y='4.55273' width='0.74659' height='3.73295' rx='0.373295' fill='#FDFDFD' />
+      <rect x='6.55078' y='12.3672' width='0.74659' height='5.59943' rx='0.373295' fill='#FDFDFD' />
+    </svg>
+  );
+};
+
+export default KitchenIcon;
