f9e42bc3427cac9c3e2318f5740958e612a821f1
Commeci <cjtkemfldj@naver.com>
2024-12-03 16:03:13 +0900

    ✨ Feat: 아이콘 커스텀 가능하도록 props 추가

--- src/components/common/icon/DateIcon.tsx
+++ src/components/common/icon/DateIcon.tsx
@@ -1,12 +1,14 @@
 interface DateIconProps {
   className?: string;
+  width?: string | number;
+  height?: string | number;
 }
 
-const DateIcon: React.FC<DateIconProps> = ({ className }) => {
+const DateIcon: React.FC<DateIconProps> = ({ className, width = 24, height = 24 }) => {
   return (
     <svg
-      width='24'
-      height='24'
+      width={width}
+      height={height}
       viewBox='0 0 24 24'
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
--- src/components/common/icon/ProfileIcon.tsx
+++ src/components/common/icon/ProfileIcon.tsx
@@ -1,12 +1,14 @@
 interface ProfileIconProps {
   className?: string;
+  width?: string | number;
+  height?: string | number;
 }
 
-const ProfileIcon: React.FC<ProfileIconProps> = ({ className }) => {
+const ProfileIcon: React.FC<ProfileIconProps> = ({ className, width = 24, height = 24 }) => {
   return (
     <svg
-      width='24'
-      height='24'
+      width={width}
+      height={height}
       viewBox='0 0 24 24'
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
