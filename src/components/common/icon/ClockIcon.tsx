b7a14dab3a57b4b0fd96ba106ac1bcdcc4d5bf22
Commeci <cjtkemfldj@naver.com>
2024-12-03 14:04:26 +0900

    ✨ Feat: props로 스타일 적용 가능하게 수정

--- src/components/common/icon/ClockIcon.tsx
+++ src/components/common/icon/ClockIcon.tsx
@@ -2,32 +2,34 @@ import React from 'react';
 
 interface ClockIconProps {
   fillClass?: string;
+  circleStrokeClass?: string;
+  handStrokeClass?: string;
+  width?: string | number;
+  height?: string | number;
 }
 
-const ClockIcon: React.FC<ClockIconProps> = ({ fillClass = 'text-main' }) => {
+const ClockIcon: React.FC<ClockIconProps> = ({
+  fillClass = 'fill-main',
+  circleStrokeClass = 'stroke-main',
+  handStrokeClass = 'stroke-white',
+  width = 24,
+  height = 24,
+}) => {
   return (
-    <svg
-      width='16'
-      height='16'
-      viewBox='0 0 24 24'
-      fill='none'
-      xmlns='http://www.w3.org/2000/svg'
-      className={fillClass}
-    >
+    <svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
       <path
         d='M20.9 12C20.9 16.9153 16.9153 20.9 12 20.9C7.08467 20.9 3.1 16.9153 3.1 12C3.1 7.08467 7.08467 3.1 12 3.1C16.9153 3.1 20.9 7.08467 20.9 12Z'
-        fill='#FDFDFD'
-        stroke='#FDFDFD'
-        strokeWidth='2.2'
-        strokeLinecap='round'
-        strokeLinejoin='round'
+        className={`${fillClass} ${circleStrokeClass}`}
+        stroke-width='2.2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
       />
       <path
         d='M12 7V12L15.3333 13.6667'
-        stroke='#1FCFBA'
-        strokeWidth='2'
-        strokeLinecap='round'
-        strokeLinejoin='round'
+        className={handStrokeClass}
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
       />
     </svg>
   );
