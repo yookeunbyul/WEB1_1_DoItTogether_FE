1f94b830380da1b3364fa0ff193af1f0aa351a17
Commeci <cjtkemfldj@naver.com>
2024-12-03 09:24:31 +0900

    💄 Style: 스타일 수정 센터로

--- src/components/common/bottomNav/BottomNav.tsx
+++ src/components/common/bottomNav/BottomNav.tsx
@@ -12,7 +12,7 @@ const BottomNav = () => {
     { name: '홈', icon: <HomeIcon /> },
     { name: '통계', icon: <ChartIcon /> },
     { name: '그룹', icon: <GroupIcon /> },
-    { name: '마이페이지', icon: <ProfileIcon /> },
+    { name: '마이', icon: <ProfileIcon /> },
   ];
 
   const handleClick = (path: string) => {
@@ -20,28 +20,38 @@ const BottomNav = () => {
   };
 
   return (
-    <div className='rounded-tr-4xl rounded-tl-4xl flex w-full max-w justify-between bg-white px-5 py-2 shadow-[0px_0px_7.2px_0px_rgba(217,250,245,1.0)]'>
-      <BottomNavBtn
-        icon={navItems[0].icon}
-        name={navItems[0].name}
-        handleClick={() => handleClick(`/main/${channelId}`)}
-      />
-      <BottomNavBtn
-        icon={navItems[1].icon}
-        name={navItems[1].name}
-        handleClick={() => handleClick(`/main/${channelId}/statistics/weekly`)}
-      />
-      <AddHouseWorkBtn handleClick={() => handleClick(`/add-housework/${channelId}/step1`)} />
-      <BottomNavBtn
-        icon={navItems[2].icon}
-        name={navItems[2].name}
-        handleClick={() => handleClick(`/main/${channelId}/group-setting`)}
-      />
-      <BottomNavBtn
-        icon={navItems[3].icon}
-        name={navItems[3].name}
-        handleClick={() => handleClick(`/main/${channelId}/my-page`)}
-      />
+    <div className='flex w-full max-w justify-between rounded-tl-4xl rounded-tr-4xl bg-white px-5 py-2 shadow-[0px_0px_7.2px_0px_rgba(217,250,245,1.0)]'>
+      <div className='flex flex-1 justify-center'>
+        <BottomNavBtn
+          icon={navItems[0].icon}
+          name={navItems[0].name}
+          handleClick={() => handleClick(`/main/${channelId}`)}
+        />
+      </div>
+      <div className='flex flex-1 justify-center'>
+        <BottomNavBtn
+          icon={navItems[1].icon}
+          name={navItems[1].name}
+          handleClick={() => handleClick(`/main/${channelId}/statistics/weekly`)}
+        />
+      </div>
+      <div className='flex flex-1 justify-center'>
+        <AddHouseWorkBtn handleClick={() => handleClick(`/add-housework/${channelId}/step1`)} />
+      </div>
+      <div className='flex flex-1 justify-center'>
+        <BottomNavBtn
+          icon={navItems[2].icon}
+          name={navItems[2].name}
+          handleClick={() => handleClick(`/main/${channelId}/group-setting`)}
+        />
+      </div>
+      <div className='flex flex-1 justify-center'>
+        <BottomNavBtn
+          icon={navItems[3].icon}
+          name={navItems[3].name}
+          handleClick={() => handleClick(`/main/${channelId}/my-page`)}
+        />
+      </div>
     </div>
   );
 };
