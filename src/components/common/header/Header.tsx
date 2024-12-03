a8c1086b679a2045d02ac799d03c505aca5b841d
yook <si89336@naver.com>
2024-12-03 15:59:24 +0900

    🐛 Fix: 오류 수정

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -21,8 +21,8 @@ const Header: React.FC<HeaderProps> = ({
     <div className='flex items-center justify-between border-b-[1px] border-solid border-gray3/40 px-5 py-4'>
       <div className='flex-1 bg-white'>{handleBack && <BackBtn handleClick={handleBack} />}</div>
       <div className='flex-1 bg-white text-center font-subhead'>{title}</div>
-      <div className='flex-1 bg-white text-end'>
-        {isNeededDoneBtn && (
+      <div className='flex flex-1 justify-end bg-white'>
+        {isNeededDoneBtn ? (
           <button className='text-black02' onClick={handleDone}>
             완료
           </button>
