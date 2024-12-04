2502baef3279d70b204aa511fcef6d5f6e1f21b3
Commeci <cjtkemfldj@naver.com>
2024-12-04 11:12:53 +0900

    💄 Style: 주간 통계 디자인 입히기

--- src/components/statistics/weekly/WeeklyRanking/WeeklyPodium/WeeklyPodium.tsx
+++ src/components/statistics/weekly/WeeklyRanking/WeeklyPodium/WeeklyPodium.tsx
@@ -9,13 +9,17 @@ const WeeklyPodium: React.FC<WeeklyPodiumProps> = ({ rank, nickname, completeCou
   // TODO 프로필 이미지 저장 후 변경
   const profile = `https://fakeimg.pl/200x200/1FCFBA,128/000,255?text=Doto`;
   return (
-    <div className={`flex flex-col items-center justify-center ${rank === 2 && 'order-first'}`}>
+    <div
+      className={`flex flex-col items-center justify-center gap-2 ${rank === 2 && 'order-first'}`}
+    >
       <div
-        className={`relative ${rank === 1 ? 'h-24 w-24' : 'h-16 w-16'} flex items-center justify-center rounded-full border border-solid bg-white`}
+        className={`relative ${rank === 1 ? 'h-24 w-24' : 'h-16 w-16'} flex items-center justify-center rounded-full border border-solid`}
       >
         <img src={profile} alt='profile' className='w-full rounded-full' />
         <div
-          className={`absolute ${rank === 1 ? '-left-0' : '-left-2'} top-0 flex h-6 w-6 items-center justify-center rounded-full bg-gray3 text-14`}
+          className={`absolute ${rank === 1 ? '-left-0' : '-left-2'} top-0 flex h-6 w-6 items-center justify-center rounded-full ${
+            rank === 1 ? 'bg-rank1' : rank === 2 ? 'bg-rank2' : rank === 3 ? 'bg-rank3' : 'bg-gray3'
+          } text-white font-label`}
         >
           {rank}
         </div>
@@ -23,7 +27,7 @@ const WeeklyPodium: React.FC<WeeklyPodiumProps> = ({ rank, nickname, completeCou
       <p>
         <strong>{nickname}</strong>
       </p>
-      <p className='rounded-full bg-gray px-2 text-14 text-white'>
+      <p className='rounded-full bg-main px-3 py-1 text-white font-label'>
         {completeCount === 0 ? '-' : `${completeCount}개`}
       </p>
     </div>
