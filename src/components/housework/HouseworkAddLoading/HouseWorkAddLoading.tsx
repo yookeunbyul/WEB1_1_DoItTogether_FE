b12f3ccc7d03a26dfb26efe490d995ef3f071895
yook <si89336@naver.com>
2024-12-04 10:20:42 +0900

    💄 Style: 집안일 추가 로딩 화면 스타일 수정

--- src/components/housework/HouseworkAddLoading/HouseWorkAddLoading.tsx
+++ src/components/housework/HouseworkAddLoading/HouseWorkAddLoading.tsx
@@ -6,6 +6,7 @@ interface HouseWorkAddLoadingProps {
   housework: string;
   member: number | null;
   members: User[];
+  category: string;
 }
 
 const HouseWorkAddLoading: React.FC<HouseWorkAddLoadingProps> = ({
@@ -13,18 +14,29 @@ const HouseWorkAddLoading: React.FC<HouseWorkAddLoadingProps> = ({
   housework,
   member,
   members,
+  category,
 }) => {
   const findNameByMember = members.find(mem => mem.userId === member)?.nickName;
   return (
     <div className='flex h-screen w-full max-w flex-col gap-4'>
-      <p className='mt-16 text-24'>{date}에</p>
+      <p className='mt-16 text-main font-title'>{date}에</p>
       <div className='flex flex-wrap items-center gap-2'>
-        <TextTag type='darkfill' label={housework} />
-        <p className='text-20'>를</p>
+        <TextTag
+          type='darkfill'
+          label={housework}
+          category={category}
+          className='flex items-center gap-x-2 font-label'
+        />
+        <p className='text-main font-head'>를</p>
       </div>
       <div className='flex flex-wrap items-center gap-2'>
-        <TextTag type='darkfill' label={findNameByMember ?? ''} />
-        <p className='text-20'>님이 담당하도록 추가했어요!</p>
+        <TextTag
+          type='darkfill'
+          label={findNameByMember ?? ''}
+          findNameByMember={findNameByMember}
+          className='flex items-center gap-x-2 font-label'
+        />
+        <p className='text-main font-head'>님이 담당하도록 추가했어요!</p>
       </div>
       <div className='flex h-40 items-center justify-center'>애니메이션 영역</div>
     </div>
