270d1fbc11bdda39dfe954b16c17b748ec1c2ee7
Commeci <cjtkemfldj@naver.com>
2024-12-04 16:51:24 +0900

    🔧 Wrench : import 추가

--- src/services/user/patchMyInitState.ts
+++ src/services/user/patchMyInitState.ts
@@ -1,8 +1,11 @@
 import { axiosInstance } from '@/services/axiosInstance';
+import { PatchMyInitStateRes } from '@/types/apis/userApi';
 
 export const patchMyInitState = async () => {
   try {
-    const response = await axiosInstance.patch(`/api/v1/users/my/profile/setup`);
+    const response = await axiosInstance.patch<PatchMyInitStateRes>(
+      `/api/v1/users/my/profile/setup`
+    );
     return response.data;
   } catch (error) {
     throw new Error('내 초기 상태 변경에 실패했습니다');
