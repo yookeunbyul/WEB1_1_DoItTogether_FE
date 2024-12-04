c881b01dd2173ca0b337a197ebdbd48dce5a7cc5
Commeci <cjtkemfldj@naver.com>
2024-12-04 16:45:34 +0900

    🔧 Wrench: 문구 수정

--- src/services/user/patchMyInitState.ts
+++ src/services/user/patchMyInitState.ts
@@ -5,6 +5,6 @@ export const patchMyInitState = async () => {
     const response = await axiosInstance.patch(`/api/v1/users/my/profile/setup`);
     return response.data;
   } catch (error) {
-    throw new Error('프로필 수정에 실패했습니다');
+    throw new Error('내 초기 상태 변경에 실패했습니다');
   }
 };
