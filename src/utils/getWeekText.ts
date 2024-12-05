d4ed42b898694ee36387c52a2e9da9fb5249c7bc
Eugeeun <jack603@naver.com>
2024-12-05 12:23:57 +0900

    🔧 Wrench: 홈 헤더 날짜 텍스트 형식 변경

--- src/utils/getWeekText.ts
+++ src/utils/getWeekText.ts
@@ -9,5 +9,19 @@ export default function getWeekText(date: Date) {
   const adjustedDay = date.getDate() + (firstDayOfWeek === 0 ? 0 : 7 - firstDayOfWeek);
   const weekNumber = Math.ceil(adjustedDay / 7);
 
-  return `${year}년 ${new Intl.DateTimeFormat('ko-KR', { month: 'long' }).format(date)} ${weekNumber}주차`;
+  // 주차 대신 "첫째 주", "둘째 주" 형식으로 반환
+  const weekText =
+    weekNumber === 1
+      ? '첫째 주'
+      : weekNumber === 2
+        ? '둘째 주'
+        : weekNumber === 3
+          ? '셋째 주'
+          : weekNumber === 4
+            ? '넷째 주'
+            : weekNumber === 5
+              ? '다섯째 주'
+              : '여섯째 주 이상';
+
+  return `${year}년 ${new Intl.DateTimeFormat('ko-KR', { month: 'long' }).format(date)} ${weekText}`;
 }
