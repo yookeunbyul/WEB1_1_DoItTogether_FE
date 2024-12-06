aeb0a5bf8c1f7e0c8232ed79fc37882fdd4bc08e
ayoung <a01259@naver.com>
2024-12-06 15:22:59 +0900

    ✨ Feat: 랭킹 > 4위부터 기본 프로필 이미지 노출처리

--- src/components/statistics/weekly/WeeklyRanking/WeeklyOthers/WeeklyOthers.tsx
+++ src/components/statistics/weekly/WeeklyRanking/WeeklyOthers/WeeklyOthers.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { WeeklyMemberScore } from '@/store/useWeeklyStatisticsStore';
+import { ProfileDefaultIcon } from '@/components/common/icon';
 
 interface WeeklyOthersProps extends WeeklyMemberScore {
   rank: number;
@@ -16,11 +17,15 @@ const WeeklyOthers: React.FC<WeeklyOthersProps> = ({
       <div className='flex items-center gap-2'>
         <p>{rank}</p>
         <div className='flex h-8 w-8 items-center justify-center overflow-hidden rounded-full'>
-          <img
-            className='h-full w-full rounded-full object-cover'
-            src={profileImageUrl}
-            alt='프로필 이미지'
-          />
+          {profileImageUrl ? (
+            <img
+              className='h-full w-full rounded-full object-cover'
+              src={profileImageUrl}
+              alt='프로필 이미지'
+            />
+          ) : (
+            <ProfileDefaultIcon />
+          )}
         </div>
         <p className='text-gray3 font-body'>{nickName}</p>
       </div>
