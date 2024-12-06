7c9bfea0ef9fe5d9f092b1c81293d933447d020f
ayoung <a01259@naver.com>
2024-12-06 10:02:16 +0900

    ✨ Feat: 에러페이지 토큰값에 따른 이동처리

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 const ErrorPage = () => {
   const navigate = useNavigate();
   const handleClick = () => {
-    navigate(-1);
+    localStorage.getItem('access_token') ? navigate('/group-select') : navigate('/');
   };
   return (
     <div className='flex h-screen flex-col items-center justify-between pb-6'>
@@ -14,7 +14,7 @@ const ErrorPage = () => {
         <div className='text-gray3 font-label'>요청하신 페이지를 찾을 수 없습니다</div>
       </div>
       <div className='w-full px-5'>
-        <Button label='메인으로 돌아가기' variant='full' size='large' handleClick={handleClick} />
+        <Button label='돌아가기' variant='full' size='large' handleClick={handleClick} />
       </div>
     </div>
   );
