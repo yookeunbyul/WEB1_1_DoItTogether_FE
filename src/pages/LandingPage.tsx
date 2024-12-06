fb8a38c8b198c26d741629159ce210ccb02a7eaf
Eugeeun <jack603@naver.com>
2024-12-06 17:02:59 +0900

    🔧 Wrench: 랜딩 페이지 리다이렉팅 수정

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -5,6 +5,11 @@ import { useNavigate } from 'react-router-dom';
 import useDeviceHeight from '@/hooks/useDevice';
 import LottieIcon from '@/components/common/lottie/LottieIcon';
 
+const apiUrl =
+  import.meta.env.MODE === 'development'
+    ? import.meta.env.VITE_API_BASE_URL // 로컬 환경
+    : import.meta.env.VITE_SERVER_URL;
+
 const LandingPage = () => {
   const navigate = useNavigate();
   const customHeightClass = useDeviceHeight();
@@ -14,7 +19,7 @@ const LandingPage = () => {
       navigate('/group-select');
       return;
     }
-    window.location.href = `${import.meta.env.VITE_SERVER_URL}/oauth2/authorization/kakao`;
+    window.location.href = `${apiUrl}/oauth2/authorization/kakao`;
   };
 
   useEffect(() => {
