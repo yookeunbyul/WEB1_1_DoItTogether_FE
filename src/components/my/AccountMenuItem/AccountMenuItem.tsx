6d59a37bdbbdfb6879fb0e490bcb3096b7f66437
Eugeeun <jack603@naver.com>
2024-12-23 19:48:34 +0900

    🐛 Fix: Ref 오류 수정

--- src/components/my/AccountMenuItem/AccountMenuItem.tsx
+++ src/components/my/AccountMenuItem/AccountMenuItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { forwardRef } from 'react';
 import { OutIcon } from '@/components/common/icon';
 import { ArrowRightIcon } from '@/components/common/icon';
 
@@ -9,30 +9,27 @@ interface AccountMenuItemProps {
   handleClick?: () => void;
 }
 
-const AccountMenuItem: React.FC<AccountMenuItemProps> = ({
-  label,
-  state,
-  iconType,
-  handleClick,
-}) => {
-  return (
-    <div className='w-full border-b border-solid border-gray4/30'>
-      <div className='flex h-12 items-center justify-between px-5'>
-        <p className='text-gray1 font-body'>{label}</p>
-        {state && <p className='text-gray1 font-label'>{state}</p>}
-        {iconType === '로그아웃' && (
-          <div className='' onClick={handleClick}>
-            <OutIcon className={'text-gray3'} />
-          </div>
-        )}
-        {iconType === '탈퇴' && (
-          <div className='' onClick={handleClick}>
-            <ArrowRightIcon className='text-gray3' />
-          </div>
-        )}
+const AccountMenuItem = forwardRef<HTMLDivElement, AccountMenuItemProps>(
+  ({ label, state, iconType, handleClick }, ref) => {
+    return (
+      <div ref={ref} className='w-full border-b border-solid border-gray4/30'>
+        <div className='flex h-12 items-center justify-between px-5'>
+          <p className='text-gray1 font-body'>{label}</p>
+          {state && <p className='text-gray1 font-label'>{state}</p>}
+          {iconType === '로그아웃' && (
+            <div className='' onClick={handleClick}>
+              <OutIcon className={'text-gray3'} />
+            </div>
+          )}
+          {iconType === '탈퇴' && (
+            <div className='' onClick={handleClick}>
+              <ArrowRightIcon className='text-gray3' />
+            </div>
+          )}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
 
 export default AccountMenuItem;
