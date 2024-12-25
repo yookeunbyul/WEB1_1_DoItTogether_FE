1d9a2bb93c867773df77552667307a44f09538ea
Eugeeun <jack603@naver.com>
2024-12-25 18:06:36 +0900

    ✨ Feat: 랜딩 페이지에 helmet 적용

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,6 +1,7 @@
 import { motion } from 'framer-motion';
 import { ServiceTitle, ServiceLogo, KakaoLoginButton } from '@/components/landing';
 import { useLanding } from '@/hooks/useLanding';
+import MetaTags from '@/components/common/metaTags/MetaTags';
 
 const LandingPage = () => {
   const { handleLoginButton } = useLanding();
@@ -16,18 +17,26 @@ const LandingPage = () => {
   };
 
   return (
-    <motion.div
-      className={`mx-auto flex h-screen flex-col gap-10 px-5 text-center`}
-      variants={container}
-      initial='hidden'
-      animate='show'
-    >
-      <div className='flex flex-1 flex-col'>
-        <ServiceTitle />
-        <ServiceLogo />
-      </div>
-      <KakaoLoginButton handleLoginButton={handleLoginButton} />
-    </motion.div>
+    <>
+      <MetaTags
+        title={'DoItTogether 두잇투게더'}
+        description={'가사 분담이 필요한 모든 이에게'}
+        url={'https://doit-together.vercel.app/'}
+      />
+
+      <motion.div
+        className={`mx-auto flex h-screen flex-col gap-10 px-5 text-center`}
+        variants={container}
+        initial='hidden'
+        animate='show'
+      >
+        <div className='flex flex-1 flex-col'>
+          <ServiceTitle />
+          <ServiceLogo />
+        </div>
+        <KakaoLoginButton handleLoginButton={handleLoginButton} />
+      </motion.div>
+    </>
   );
 };
 
