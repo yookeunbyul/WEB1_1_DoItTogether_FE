c62f486549f5d4a809e776a6e77292808d6686b2
Eugeeun <jack603@naver.com>
2024-12-25 19:42:50 +0900

    ✨ Feat: 메인 페이지 helmet 추가

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -8,6 +8,8 @@ import {
   NoList,
   WeeklyDateAndTab,
 } from '@/components/home';
+import MetaTags from '@/components/common/metaTags/MetaTags';
+import { useParams } from 'react-router-dom';
 
 const HomePage: React.FC = () => {
   const {
@@ -19,9 +21,15 @@ const HomePage: React.FC = () => {
     handleEdit,
     handleDelete,
   } = useHomePage();
+  const { channelId } = useParams();
 
   return (
-    <div>
+    <>
+      <MetaTags
+        title={'두잇투게더 - 메인'}
+        description={'등록된 집안일을 확인하고 관리해보세요.'}
+        url={`https://doit-together.vercel.app/main/${channelId}/`}
+      />
       <HomeHeader />
       <WeeklyDateAndTab
         activeTab={activeTab}
@@ -39,7 +47,7 @@ const HomePage: React.FC = () => {
         />
       )}
       <GroupSelectSheet />
-    </div>
+    </>
   );
 };
 
