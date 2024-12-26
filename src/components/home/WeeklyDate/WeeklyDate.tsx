7e51b886d8bd3137347cb04259dfe6b5099ce00c
Eugeeun <jack603@naver.com>
2024-12-27 00:17:16 +0900

    ✨ Feat: 메인 페이지 지연 로딩 적용

--- src/components/home/WeeklyDate/WeeklyDate.tsx
+++ src/components/home/WeeklyDate/WeeklyDate.tsx
@@ -1,13 +1,13 @@
-import { useState, useEffect, useRef } from 'react';
-import DateItem from './DateItem/DateItem';
+import React, { useState, useEffect, useRef, useCallback, useMemo, Suspense } from 'react';
 import { Carousel, CarouselContent, CarouselItem } from '@/components/common/ui/carousel';
 import getWeekText from '@/utils/getWeekText';
 import useHomePageStore from '@/store/useHomePageStore';
 import { type CarouselApi } from '@/components/common/ui/carousel';
-import { IncompleteScoreResponse } from '@/types/apis/houseworkApi';
 import { getWeeklyIncomplete } from '@/services/housework/getWeeklyIncomplete';
 import { useParams } from 'react-router-dom';
 import getFormattedDate from '@/utils/getFormattedDate';
+
+const DateItem = React.lazy(() => import('./DateItem/DateItem'));
 
 const WeeklyDate = () => {
   const [api, setApi] = useState<CarouselApi>();
@@ -24,6 +24,32 @@ const WeeklyDate = () => {
   const { channelId } = useParams();
   const prevChannelIdRef = useRef(channelId);
 
+  const fetchCurrWeek = useCallback(
+    async (newDate: string) => {
+      try {
+        const newChannelId = Number(channelId);
+        const currWeekResult = await getWeeklyIncomplete({
+          channelId: newChannelId,
+          targetDate: newDate,
+        });
+        const weekdays = ['일', '월', '화', '수', '목', '금', '토'];
+        const newWeekDates = currWeekResult.result.incompleteScoreResponses.map(data => {
+          const date = new Date(data.date);
+          const weekdayIndex = date.getDay();
+          return {
+            ...data,
+            day: weekdays[weekdayIndex],
+          };
+        });
+
+        setCurrWeek(newWeekDates);
+      } catch (error) {
+        console.error('주간 미완료율 조회 실패:', error);
+      }
+    },
+    [channelId, setCurrWeek]
+  );
+
   useEffect(() => {
     if (prevChannelIdRef.current !== channelId) {
       const today = new Date();
@@ -33,11 +59,11 @@ const WeeklyDate = () => {
       setActiveDate(getFormattedDate(today));
       prevChannelIdRef.current = channelId;
     }
-  }, [channelId]);
+  }, [channelId, fetchCurrWeek, setActiveDate, setActiveWeek, setWeekText]);
 
   useEffect(() => {
     fetchCurrWeek(getFormattedDate(activeWeek));
-  }, [activeWeek]);
+  }, [activeWeek, fetchCurrWeek]);
 
   useEffect(() => {
     if (!api) return;
@@ -53,34 +79,7 @@ const WeeklyDate = () => {
       }
       setCurrent(newCurrent);
     });
-  }, [api, current, activeWeek]);
-
-  const fetchCurrWeek = async (newDate: string) => {
-    try {
-      const newChannelId = Number(channelId);
-      const currWeekResult = await getWeeklyIncomplete({
-        channelId: newChannelId,
-        targetDate: newDate,
-      });
-      const weekdays = ['일', '월', '화', '수', '목', '금', '토'];
-      const newWeekDates = (weekData: IncompleteScoreResponse[]) => {
-        return weekData.map(data => {
-          const date = new Date(data.date);
-          const weekdayIndex = date.getDay();
-          const day = weekdays[weekdayIndex];
-
-          return {
-            ...data,
-            day,
-          };
-        });
-      };
-
-      setCurrWeek(newWeekDates(currWeekResult.result.incompleteScoreResponses));
-    } catch (error) {
-      console.error('주간 미완료율 조회 실패:', error);
-    }
-  };
+  }, [api, current]);
 
   const handleActiveDate = async (date: string) => {
     setActiveDate(date);
@@ -88,16 +87,13 @@ const WeeklyDate = () => {
 
   const changeWeek = (direction: 'next' | 'previous') => {
     const newDate = new Date(activeWeek);
-
-    if (direction === 'next') {
-      newDate.setDate(activeWeek.getDate() + 7);
-    } else {
-      newDate.setDate(activeWeek.getDate() - 7);
-    }
+    newDate.setDate(activeWeek.getDate() + (direction === 'next' ? 7 : -7));
 
     setActiveWeek(newDate);
     setWeekText(getWeekText(newDate));
   };
+
+  const memoizedCurrWeek = useMemo(() => currWeek, [currWeek]);
 
   return (
     <Carousel opts={{ loop: true }} setApi={setApi}>
@@ -105,17 +101,19 @@ const WeeklyDate = () => {
         {[...Array(3)].map((_, i) => (
           <CarouselItem key={i}>
             <div className='flex touch-none items-center justify-center gap-3 bg-white px-5 py-2'>
-              {currWeek.map((week, idx) => (
-                <DateItem
-                  key={idx}
-                  date={week.date.split('-')[2]}
-                  day={week.day}
-                  pendingCnt={week.houseworkIncompleteCount}
-                  solvedMatters={week.solvedMatters}
-                  isActive={activeDate === week.date && current === i}
-                  handleClick={() => handleActiveDate(week.date)}
-                />
-              ))}
+              <Suspense fallback={<div></div>}>
+                {memoizedCurrWeek.map(week => (
+                  <DateItem
+                    key={week.date} // 고유한 키 사용
+                    date={week.date.split('-')[2]}
+                    day={week.day}
+                    pendingCnt={week.houseworkIncompleteCount}
+                    solvedMatters={week.solvedMatters}
+                    isActive={activeDate === week.date && current === i}
+                    handleClick={() => handleActiveDate(week.date)}
+                  />
+                ))}
+              </Suspense>
             </div>
           </CarouselItem>
         ))}
