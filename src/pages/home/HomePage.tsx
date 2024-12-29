2ac4fdc25db077959fc1fc27ed6e38724f401534
Eugeeun <jack603@naver.com>
2024-12-29 20:21:35 +0900

    ♻️ Refactor: 메인페이지 렌더링 최적화

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { useHomePage } from '@/hooks/useHomePage';
-
-import {
-  GroupSelectSheet,
-  HomeHeader,
-  HouseworkList,
-  NoList,
-  WeeklyDateAndTab,
-} from '@/components/home';
 import MetaTags from '@/components/common/metaTags/MetaTags';
 import { useParams } from 'react-router-dom';
+
+const GroupSelectSheet = lazy(() => import('@/components/home/GroupSelectSheet/GroupSelectSheet'));
+const HomeHeader = lazy(() => import('@/components/home/HomeHeader/HomeHeader'));
+const HouseworkList = lazy(() => import('@/components/home/HouseworkList/HouseworkList'));
+const NoList = lazy(() => import('@/components/home/NoList/NoList'));
+const WeeklyDateAndTab = lazy(() => import('@/components/home/WeeklyDateAndTab'));
 
 const HomePage: React.FC = () => {
   const {
@@ -30,23 +28,26 @@ const HomePage: React.FC = () => {
         description={'등록된 집안일을 확인하고 관리해보세요.'}
         url={`https://doit-together.vercel.app/main/${channelId}/`}
       />
-      <HomeHeader />
-      <WeeklyDateAndTab
-        activeTab={activeTab}
-        handleSetActiveTab={setActiveTab}
-        chargers={chargers}
-      />
-      {!filteredHouseworks || filteredHouseworks.length === 0 ? (
-        <NoList />
-      ) : (
-        <HouseworkList
-          items={filteredHouseworks}
-          handleAction={handleAction}
-          handleEdit={handleEdit}
-          handleDelete={handleDelete}
+
+      <Suspense fallback={<div></div>}>
+        <HomeHeader />
+        <WeeklyDateAndTab
+          activeTab={activeTab}
+          handleSetActiveTab={setActiveTab}
+          chargers={chargers}
         />
-      )}
-      <GroupSelectSheet />
+        {!filteredHouseworks || filteredHouseworks.length === 0 ? (
+          <NoList />
+        ) : (
+          <HouseworkList
+            items={filteredHouseworks}
+            handleAction={handleAction}
+            handleEdit={handleEdit}
+            handleDelete={handleDelete}
+          />
+        )}
+        <GroupSelectSheet />
+      </Suspense>
     </>
   );
 };
