8c15cb77fb4665ea3bce52be50465697d82ff092
yook <si89336@naver.com>
2025-01-02 18:36:18 +0900

    ⚡️ Improve: 전역변수 관리 및 persist 설정정

--- src/store/useAddHouseWorkStore.ts
+++ src/store/useAddHouseWorkStore.ts
@@ -1,35 +1,53 @@
+import { Housework } from '@/types/apis/houseworkApi';
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 interface AddHouseWorkState {
-  isAllday: boolean;
   userId: number | null;
   selectedItem: number | null;
   selectedDate: Date | undefined;
+  nickname: string;
+  targetHousework: Housework | undefined;
 }
 
 interface AddHouseWorkActions {
-  setIsAllday: (value: boolean) => void;
   setUserId: (id: number | null) => void;
   setSelectedItem: (itemId: number | null) => void;
   setSelectedDate: (date: Date | undefined) => void;
+  setNickName: (nickname: string) => void;
+  setTargetHousework: (targetHousework: Housework | undefined) => void;
   reset: () => void;
 }
 
 const initialState: AddHouseWorkState = {
-  isAllday: true,
   userId: null,
   selectedItem: null,
   selectedDate: undefined,
+  nickname: '',
+  targetHousework: undefined,
 };
 
-const useAddHouseWorkStore = create<AddHouseWorkState & AddHouseWorkActions>(set => ({
-  ...initialState,
+type State = AddHouseWorkState & AddHouseWorkActions;
+type PersistedState = Pick<AddHouseWorkState, 'targetHousework'>;
 
-  setIsAllday: isAllday => set({ isAllday }),
-  setUserId: userId => set({ userId }),
-  setSelectedItem: selectedItem => set({ selectedItem }),
-  setSelectedDate: selectedDate => set({ selectedDate }),
-  reset: () => set(initialState),
-}));
+const useAddHouseWorkStore = create<State>()(
+  persist(
+    set => ({
+      ...initialState,
+      setUserId: userId => set({ userId }),
+      setSelectedItem: selectedItem => set({ selectedItem }),
+      setSelectedDate: selectedDate => set({ selectedDate }),
+      setNickName: nickname => set({ nickname }),
+      setTargetHousework: targetHousework => set({ targetHousework }),
+      reset: () => set(initialState),
+    }),
+    {
+      name: 'housework-storage',
+      partialize: (state): PersistedState => ({
+        targetHousework: state.targetHousework,
+      }),
+    }
+  )
+);
 
 export default useAddHouseWorkStore;
