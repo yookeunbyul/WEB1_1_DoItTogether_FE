b03801f6dc648953d47ec1b8ef10f12d43a1e1c5
Commeci <cjtkemfldj@naver.com>
2025-01-03 13:29:57 +0900

    ♻️ Refactor: 최적화

--- src/hooks/useRegister.ts
+++ src/hooks/useRegister.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { getMyInfo } from '@/services/user/getMyInfo';
 import { patchMyInfo } from '@/services/user/patchMyInfo';
@@ -10,40 +10,48 @@ export const useRegister = () => {
   const [profileUrl, setProfileUrl] = useState('');
   const [error, setError] = useState<boolean>(false);
 
+  const nicknameRegex = useMemo(() => INPUT_VALIDATION.nickname.regexp, []);
+  const maxLength = useMemo(() => INPUT_VALIDATION.nickname.maxLength, []);
+
+  // 닉네임 유효성 검사
+  const validateNickname = useCallback(
+    (value: string) => {
+      return value.length <= maxLength && nicknameRegex.test(value);
+    },
+    [maxLength, nicknameRegex]
+  );
+
+  const fetchUserInfo = useCallback(async () => {
+    try {
+      const response = await getMyInfo();
+      setName(response.result.nickName);
+      setProfileUrl(response.result.profileImageUrl);
+      setError(!validateNickname(response.result.nickName));
+    } catch (error) {
+      console.error('내 정보 조회 실패:', error);
+    }
+  }, [validateNickname]);
+
   useEffect(() => {
-    const fetchUserInfo = async () => {
-      try {
-        const response = await getMyInfo();
-        setName(response.result.nickName);
-        setProfileUrl(response.result.profileImageUrl);
-      } catch (error) {
-        console.error('내 정보 조회 실패:', error);
-      }
-    };
-
     fetchUserInfo();
-  }, []);
+  }, [fetchUserInfo]);
 
-  const handleSubmitButton = async () => {
+  const handleSubmitButton = useCallback(async () => {
     try {
       await patchMyInfo({ nickName: name });
       navigate('/survey-intro');
     } catch (error) {
       console.error('프로필 수정 및 초기 정보 받기 실패:', error);
     }
-  };
+  }, [name, navigate]);
 
-  const handleNameChange = (value: string) => {
-    setName(value);
-    if (
-      value.length <= INPUT_VALIDATION.nickname.maxLength &&
-      INPUT_VALIDATION.nickname.regexp.test(value)
-    ) {
-      setError(false);
-    } else {
-      setError(true);
-    }
-  };
+  const handleNameChange = useCallback(
+    (value: string) => {
+      setName(value);
+      setError(!validateNickname(value));
+    },
+    [validateNickname]
+  );
 
   return {
     name,
