d500ca3469ba47151cd44cf93e6415585dea5b45
Commeci <cjtkemfldj@naver.com>
2025-01-13 18:32:02 +0900

    🔧 Wrench: 분류하기 어려운 수정

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.c0a2592n9b"
+    "revision": "0.3m95jhlhoro"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
